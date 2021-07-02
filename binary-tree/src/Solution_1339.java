9c53713147cf68d659a0d24d9d0c464513c0ae70
bharat <bharatarora3072@gmail.com>
2021-07-02 12:06:19 +0530

    Fix 1.0

---
+++ binary-tree/src/Solution_1339.java
@@ -0,0 +1,6 @@
+/**
+ * https://leetcode.com/problems/maximum-product-of-splitted-binary-tree/
+ * TODO
+ */
+class Solution_1339 {
+}
---
+++ binary-tree/src/Solution_1443.java
@@ -0,0 +1,6 @@
+/**
+ * https://leetcode.com/problems/minimum-time-to-collect-all-apples-in-a-tree/
+ * TODO
+ */
+class Solution_1443 {
+}
