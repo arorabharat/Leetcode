0a99f45c046aee74a4cccdc0f6c091d7e6600f78
bharat <bharatarora3072@gmail.com>
2021-07-06 15:22:27 +0530

    Tue Jul  6 15:22:27 IST 2021

--- binary-tree/src/Solution_113.java
+++ binary-tree/src/Solution_113.java
@@ -1,6 +1,66 @@
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.List;
+
 /**
  * https://leetcode.com/problems/path-sum-ii/
- * TODO
  */
 class Solution_113 {
+
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public List<Deque<Integer>> _pathSum(TreeNode root, int targetSum) {
+        if (root == null) {
+            return new LinkedList<>();
+        }
+        if (root.left == null && root.right == null) {
+            if (root.val == targetSum) {
+                List<Deque<Integer>> pathList = new LinkedList<>();
+                Deque<Integer> path = new LinkedList<>();
+                path.add(root.val);
+                pathList.add(path);
+                return pathList;
+            }
+            return new LinkedList<>();
+        }
+        List<Deque<Integer>> pathLeftList = _pathSum(root.left, targetSum - root.val);
+        List<Deque<Integer>> pathRightList = _pathSum(root.right, targetSum - root.val);
+        List<Deque<Integer>> pathRoot = new LinkedList<>();
+        for (Deque<Integer> path : pathLeftList) {
+            path.addFirst(root.val);
+            pathRoot.add(path);
+        }
+        for (Deque<Integer> path : pathRightList) {
+            path.addFirst(root.val);
+            pathRoot.add(path);
+        }
+        return pathRoot;
+    }
+
+    public List<List<Integer>> pathSum(TreeNode root, int targetSum) {
+        List<Deque<Integer>> pathDeque = _pathSum(root, targetSum);
+        List<List<Integer>> pathList = new ArrayList<>();
+        for (Deque<Integer> path : pathDeque) {
+            pathList.add(new ArrayList<>(path));
+        }
+        return pathList;
+    }
 }
