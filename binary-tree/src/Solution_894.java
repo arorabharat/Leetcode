4bd96446450155ff17e7f5d8bc26a08e9d040d22
bharat <bharatarora3072@gmail.com>
2021-07-06 15:07:22 +0530

    Tue Jul  6 15:07:22 IST 2021

--- binary-tree/src/Solution_894.java
+++ binary-tree/src/Solution_894.java
@@ -1,6 +1,31 @@
+import java.util.List;
+
 /**
  * https://leetcode.com/problems/all-possible-full-binary-trees/
- * TODO
+ * TODO : tough
  */
 class Solution_894 {
+
+    public List<TreeNode> allPossibleFBT(int n) {
+        return null;
+    }
+
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
 }
