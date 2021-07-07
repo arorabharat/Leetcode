44e33841483a6350348680dccca2a3270c9dd487
bharat <bharatarora3072@gmail.com>
2021-07-07 10:21:54 +0530

    Wed Jul  7 10:21:54 IST 2021

--- binary-tree/src/Solution_145.java
+++ binary-tree/src/Solution_145.java
@@ -1,9 +1,7 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 /**
  * https://leetcode.com/problems/binary-tree-postorder-traversal/
- * TODO
  */
 class Solution_145 {
 
@@ -26,6 +24,10 @@ class Solution_145 {
         }
     }
 
+    /**
+     * ================== PostOrder / Post Order Traversal ===============================
+     * Recursive
+     */
     private void _postorderTraversal(TreeNode root, List<Integer> postOrderList) {
         if (root != null) {
             _postorderTraversal(root.left, postOrderList);
@@ -37,6 +39,32 @@ class Solution_145 {
     public List<Integer> postorderTraversal(TreeNode root) {
         List<Integer> postOrderList = new ArrayList<>();
         _postorderTraversal(root, postOrderList);
+        return postOrderList;
+    }
+
+    /**
+     * Iterative
+     */
+    public List<Integer> postorderTraversal1(TreeNode root) {
+        List<Integer> postOrderList = new ArrayList<>();
+        if (root == null) return new ArrayList<>();
+        Stack<TreeNode> stack = new Stack<>();
+        stack.add(root);
+        Set<TreeNode> visited = new HashSet<>();
+        visited.add(root);
+        while (!stack.isEmpty()) {
+            TreeNode front = stack.pop();
+            if ( front.left != null && !visited.contains(front.left)){
+                stack.add(front.left);
+                visited.add(front.left);
+            }
+            if (front.right != null && !visited.contains(front.right)){
+                stack.add(front.right);
+                visited.add(front.right);
+            }
+            postOrderList.add(front.val);
+        }
+        Collections.reverse(postOrderList);
         return postOrderList;
     }
 }
