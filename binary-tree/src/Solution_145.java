ff6775dca0bc98e245517171c7c0d805c84ad5a4
bharat <bharatarora3072@gmail.com>
2021-07-07 10:00:20 +0530

    Wed Jul  7 10:00:20 IST 2021

--- binary-tree/src/Solution_145.java
+++ binary-tree/src/Solution_145.java
@@ -1,6 +1,42 @@
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * https://leetcode.com/problems/binary-tree-postorder-traversal/
  * TODO
  */
 class Solution_145 {
+
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    private void _postorderTraversal(TreeNode root, List<Integer> postOrderList) {
+        if (root != null) {
+            _postorderTraversal(root.left, postOrderList);
+            _postorderTraversal(root.right, postOrderList);
+            postOrderList.add(root.val);
+        }
+    }
+
+    public List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> postOrderList = new ArrayList<>();
+        _postorderTraversal(root, postOrderList);
+        return postOrderList;
+    }
 }
