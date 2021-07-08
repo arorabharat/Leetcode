1ce06973e25034c4d3aca0efa425aeb1370f31bc
bharat <bharatarora3072@gmail.com>
2021-07-08 17:04:07 +0530

    Thu Jul  8 17:04:07 IST 2021

---
+++ binary-tree/src/Solution_536.java
@@ -0,0 +1,29 @@
+/**
+ * https://leetcode.com/problems/construct-binary-tree-from-string/
+ * TODO : important
+ */
+class Solution_536 {
+
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public TreeNode str2tree(String s) {
+        return null;
+    }
+}
