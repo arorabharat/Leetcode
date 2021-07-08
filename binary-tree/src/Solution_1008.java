9f1804aecbf068c8299aa770f6c2c0b35ba1c0f3
bharat <bharatarora3072@gmail.com>
2021-07-08 16:47:03 +0530

    Thu Jul  8 16:47:03 IST 2021

---
+++ binary-tree/src/Solution_1008.java
@@ -0,0 +1,53 @@
+/**
+ * https://leetcode.com/problems/construct-binary-search-tree-from-preorder-traversal/
+ *
+ * @see Solution_109
+ * @see Solution_1007
+ * @see Solution_1654
+ */
+class Solution_1008 {
+
+    private int i;
+
+    TreeNode deserialize(int[] preorder, int start, int end) {
+        if (i == preorder.length) return null;
+        int val = preorder[i];
+        if (start < val && val < end) {
+            TreeNode root = new TreeNode(val);
+            i++;
+            root.left = deserialize(preorder, start, root.val);
+            root.right = deserialize(preorder, root.val, end);
+            return root;
+        }
+        return null;
+    }
+
+    public TreeNode bstFromPreorder(int[] preorder) {
+        if (preorder.length == 0) {
+            return null;
+        }
+        i = 0;
+        return deserialize(preorder, Integer.MIN_VALUE, Integer.MAX_VALUE);
+    }
+
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+
+}
---
+++ binary-tree/src/Solution_1654.java
@@ -0,0 +1,6 @@
+/**
+ * https://leetcode.com/problems/minimum-jumps-to-reach-home/
+ * TODO
+ */
+class Solution_1654 {
+}
