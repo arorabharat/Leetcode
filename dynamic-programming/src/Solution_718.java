07af41e3d31c6e06cf9de9670efb06f75a59659b
bharat <bharatarora3072@gmail.com>
2021-07-10 20:38:15 +0530

    Sat Jul 10 20:38:15 IST 2021

--- dynamic-programming/src/Solution_718.java
+++ dynamic-programming/src/Solution_718.java
@@ -53,21 +53,13 @@ class Solution_718 {
         int n = A.length;
         int m = B.length;
         int[][] dp = new int[n + 1][m + 1];
-
+        int ans = 0;
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
                 if (match(A[i], B[j])) {
                     dp[i + 1][j + 1] = dp[i][j] + 1;
-                } else {
-                    dp[i + 1][j + 1] = 0;
                 }
-            }
-        }
-
-        int ans = 0;
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= m; j++) {
-                ans = Math.max(ans, dp[i][j]);
+                ans = Math.max(ans, dp[i + 1][j + 1]);
             }
         }
         return ans;
