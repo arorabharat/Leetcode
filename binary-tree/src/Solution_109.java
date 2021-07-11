bf838b460595d9a78d0e21bab6a2f3abcf81d710
bharat <bharatarora3072@gmail.com>
2021-07-11 15:36:07 +0530

    Sun Jul 11 15:36:07 IST 2021

--- binary-tree/src/Solution_109.java
+++ binary-tree/src/Solution_109.java
@@ -3,4 +3,69 @@
  * TODO
  */
 class Solution_109 {
+
+    static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    ListNode getMidNode(ListNode start) {
+        ListNode slow = new ListNode(-1);
+        slow.next = start;
+        ListNode fast = slow;
+        ListNode prev = slow;
+        while (fast != null && fast.next != null && slow != null) {
+            fast = fast.next.next;
+            prev = slow;
+            slow = slow.next;
+
+        }
+        return fast == null ? prev : prev.next;
+    }
+
+    public TreeNode sortedListToBST(ListNode head) {
+        if (head == null) return null;
+        if (head.next == null) return new TreeNode(head.val);
+        ListNode leftNode = getMidNode(head);
+        ListNode rootNode = leftNode.next;
+        ListNode rightNode = leftNode.next.next;
+        rootNode.next = null;
+        leftNode.next = null;
+        TreeNode root = new TreeNode(rootNode.val);
+        root.left = sortedListToBST(head);
+        root.right = sortedListToBST(rightNode);
+        return root;
+    }
+    
 }
