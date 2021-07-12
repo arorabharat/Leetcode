e17772c43e4f2e3e525f6a36e2cbf36ea9f28808
bharat <bharatarora3072@gmail.com>
2021-07-12 22:37:36 +0530

    Mon Jul 12 22:37:36 IST 2021

---
+++ algo-design/src/Solution_362.java
@@ -0,0 +1,26 @@
+/**
+ * https://leetcode.com/problems/design-hit-counter/
+ */
+class Solution_362 {
+
+    static class HitCounter {
+
+        /** Initialize your data structure here. */
+        public HitCounter() {
+
+        }
+
+        /** Record a hit.
+         @param timestamp - The current timestamp (in seconds granularity). */
+        public void hit(int timestamp) {
+        }
+
+        /** Return the number of hits in the past 5 minutes.
+         @param timestamp - The current timestamp (in seconds granularity). */
+        public int getHits(int timestamp) {
+            return 0;
+        }
+    }
+}
+
+
