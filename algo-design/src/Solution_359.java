9aa3b918e719022d8326130e1351bdcd9e1da73c
bharat <bharatarora3072@gmail.com>
2021-07-12 22:30:23 +0530

    Mon Jul 12 22:30:23 IST 2021

---
+++ algo-design/src/Solution_359.java
@@ -0,0 +1,35 @@
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * https://leetcode.com/problems/logger-rate-limiter/solution/
+ * TODO : implement it correctly again, current solution is not optimal
+ */
+class Solution_359 {
+
+    static class Logger {
+
+        Map<String, Integer> messageToTimeMap;
+
+        public Logger() {
+            this.messageToTimeMap = new HashMap<>();
+        }
+
+        public boolean shouldPrintMessage(int timestamp, String message) {
+            if (!this.messageToTimeMap.containsKey(message)) {
+                this.messageToTimeMap.put(message, timestamp + 10);
+                return true;
+            } else {
+                int lastTimeStamp = this.messageToTimeMap.get(message);
+                if (timestamp >= lastTimeStamp) {
+                    this.messageToTimeMap.put(message, timestamp);
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+        }
+    }
+}
+
+
