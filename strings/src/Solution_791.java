82296ebfac3ddcf69194c7a0968dbc917e7fbd31
bharat <bharatarora3072@gmail.com>
2021-07-18 17:10:38 +0530

    Sun Jul 18 17:10:38 IST 2021

---
+++ strings/src/Solution_791.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+/**
+ *
+ */
+class Solution_791 {
+
+    public String customSortString(String order, String s) {
+        int n = 26;
+        int[] arr = new int[n];
+        int priority = 0;
+        for (int i = 0; i < order.length(); i++) {
+            if (arr[order.charAt(i) - 'a'] == 0) {
+                priority++;
+                arr[order.charAt(i) - 'a'] = priority;
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            if (arr[i] == 0) {
+                priority++;
+                arr[i] = priority;
+            }
+        }
+        List<Character> chars = new ArrayList<>();
+        for (int i = 0; i < s.length(); i++) {
+            chars.add(s.charAt(i));
+        }
+        chars.sort(Comparator.comparingInt(c -> arr[c - 'a']));
+        StringBuilder sb = new StringBuilder();
+        for (Character character : chars) {
+            sb.append(character);
+        }
+        return sb.toString();
+    }
+}
