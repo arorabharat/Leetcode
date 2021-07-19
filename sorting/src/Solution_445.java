9666439bc9b9c3459106a4f2ded10d10aeba9ab9
bharat <bharatarora3072@gmail.com>
2021-07-19 20:56:47 +0530

    Mon Jul 19 20:56:47 IST 2021

---
+++ sorting/src/Solution_445.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+
+/**
+ * https://leetcode.com/problems/assign-cookies/
+ */
+class Solution_445 {
+
+    public int findContentChildren(int[] g, int[] s) {
+        Arrays.sort(g);
+        Arrays.sort(s);
+        int cookieIndex = 0;
+        int content = 0;
+        int kidIndex = 0;
+        while (kidIndex < g.length) {
+            if (cookieIndex == s.length) {
+                break;
+            } else if (s[cookieIndex] >= g[kidIndex]) {
+                content++;
+                kidIndex++;
+                cookieIndex++;
+            } else {
+                cookieIndex++;
+            }
+        }
+        return content;
+    }
+}
