3e3691b63bc766655a02cdb07945375d4b685d2e
bharat <bharatarora3072@gmail.com>
2021-07-19 16:57:30 +0530

    Mon Jul 19 16:57:30 IST 2021

--- heap/src/Solution_347.java
+++ heap/src/Solution_347.java
@@ -1,7 +1,4 @@
-import java.util.Comparator;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.PriorityQueue;
+import java.util.*;
 
 /**
  * https://leetcode.com/problems/top-k-frequent-elements/
@@ -32,5 +29,43 @@ class Solution_347 {
             ans[j] = front[0];
         }
         return ans;
+    }
+
+    /**
+     * Approach 2
+     * Slight optimisation over approach 1
+     */
+    public int[] topKFrequent2(int[] nums, int k) {
+        // O(1) time
+        if (k == nums.length) {
+            return nums;
+        }
+
+        // 1. build hash map : character and how often it appears
+        // O(N) time
+        Map<Integer, Integer> count = new HashMap();
+        for (int n : nums) {
+            count.put(n, count.getOrDefault(n, 0) + 1);
+        }
+
+        // init minHeap 'the less frequent element first'
+        Queue<Integer> minHeap = new PriorityQueue<>(Comparator.comparingInt(count::get));
+
+        // 2. keep k top frequent elements in the minHeap
+        // O(N log k) < O(N log N) time
+        for (int n : count.keySet()) {
+            minHeap.add(n);
+            if (minHeap.size() > k) {
+                minHeap.poll();
+            }
+        }
+
+        // 3. build an output array
+        // O(k log k) time
+        int[] top = new int[k];
+        for (int i = k - 1; i >= 0; --i) {
+            top[i] = minHeap.poll();
+        }
+        return top;
     }
 }
