58d35c6c7da9be0cd9d053995a1176814f4da48e
bharat <bharatarora3072@gmail.com>
2021-07-20 00:55:17 +0530

    Tue Jul 20 00:55:17 IST 2021

--- sorting/src/Solution_350.java
+++ sorting/src/Solution_350.java
@@ -53,4 +53,30 @@ class Solution_350 {
         }
         return Arrays.copyOfRange(nums1, 0, k);
     }
+
+    /**
+     * Approach 3 : using sorting
+     */
+    public int[] intersect3(int[] nums1, int[] nums2) {
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+        List<Integer> list = new ArrayList<>();
+        int i = 0, j = 0;
+        while (i < nums1.length && j < nums2.length) {
+            if (nums1[i] < nums2[j]) {
+                i++;
+            } else if (nums1[i] > nums2[j]) {
+                j++;
+            } else {
+                list.add(nums1[i]);
+                i++;
+                j++;
+            }
+        }
+        int[] arr = new int[list.size()];
+        for (int k = 0; k < arr.length; k++) {
+            arr[k] = list.get(k);
+        }
+        return arr;
+    }
 }
