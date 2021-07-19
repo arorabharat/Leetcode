e25d79cd22de7aea18ed8cdbefb92c6b0ef1b26c
bharat <bharatarora3072@gmail.com>
2021-07-20 00:30:57 +0530

    Tue Jul 20 00:30:57 IST 2021

--- two-pointer/src/Solution_15.java
+++ two-pointer/src/Solution_15.java
@@ -76,16 +76,42 @@ class Solution_15 {
     }
 
     void twoSum(int[] nums, int i, List<List<Integer>> res) {
-        var seen = new HashSet<Integer>();
+        Set<Integer> seen = new HashSet<>();
         for (int j = i + 1; j < nums.length; ++j) {
             int complement = -nums[i] - nums[j];
             if (seen.contains(complement)) {
                 res.add(Arrays.asList(nums[i], nums[j], complement));
-                while (j + 1 < nums.length && nums[j] == nums[j + 1]){
+                while (j + 1 < nums.length && nums[j] == nums[j + 1]) {
                     ++j;
                 }
             }
             seen.add(nums[j]);
         }
+    }
+
+    /**
+     * Approach 3 : using hashset˳
+     * @see Level#GOOD
+     * Java set internally compare the complete list
+     */
+    public List<List<Integer>> threeSum4(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        Set<List<Integer>> unique = new HashSet<>();
+        for (int i = 0; i < nums.length; ++i) {
+            Set<Integer> seen = new HashSet<>();
+            for (int j = i + 1; j < nums.length; ++j) {
+                int complement = -nums[i] - nums[j];
+                if (seen.contains(complement)) {
+                    List<Integer> triplet = Arrays.asList(nums[i], nums[j], complement);
+                    Collections.sort(triplet);
+                    if (!unique.contains(triplet)) {
+                        res.add(triplet);
+                        unique.add(triplet);
+                    }
+                }
+                seen.add(nums[j]);
+            }
+        }
+        return res;
     }
 }
