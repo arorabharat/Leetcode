88f2231ddabcb64f487fcc96bde4692c2fc5fa33
bharat <bharatarora3072@gmail.com>
2021-07-19 19:03:51 +0530

    Mon Jul 19 19:03:51 IST 2021

--- commands.sh
+++ commands.sh
@@ -1,10 +1,17 @@
 ## Return the number of question with Solution_ pattern
 function count_leetcode_questions() {
+    echo '========================================================'
+    echo 'Total number of questions :'
     ls -R | grep 'Solution_' | wc -l
+    echo '========================================================'
 }
 
+## Returns the list of duplicate questions
 function get_duplicate_questions() {
+    echo '========================================================'
+    echo 'List of Duplicate questions :'
     ls -R | grep Solution_ | sort | uniq -c | grep -v '1 Solution_'
+    echo '========================================================'
 }
 
 ## run the method
