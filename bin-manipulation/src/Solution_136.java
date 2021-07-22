c7ede4390d803dd0eac8365be37efa426899bf70
bharat <bharatarora3072@gmail.com>
2021-07-22 22:22:39 +0530

    Thu Jul 22 22:22:39 IST 2021

---
+++ bin-manipulation/src/Solution_136.java
@@ -0,0 +1,16 @@
+/**
+ * https://leetcode.com/problems/single-number/
+ * @see Solution_137
+ * @see Solution_268
+ * @see Solution_389
+ */
+class Solution_136 {
+
+    public int singleNumber(int[] nums) {
+        int xor = 0;
+        for (int num : nums) {
+            xor = xor ^ num;
+        }
+        return xor;
+    }
+}
---
+++ bin-manipulation/src/Solution_268.java
@@ -0,0 +1,5 @@
+/**
+ * https://leetcode.com/problems/missing-number/
+ */
+class Solution_268 {
+}
