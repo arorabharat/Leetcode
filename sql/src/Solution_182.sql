b85336fb475d06075d42b2388d5e34d773eb8a72
bharat <bharatarora3072@gmail.com>
2021-07-23 15:32:24 +0530

    Fri Jul 23 15:32:24 IST 2021

--- sql/src/Solution_182.sql
+++ sql/src/Solution_182.sql
@@ -1,5 +1,16 @@
 # https://leetcode.com/problems/duplicate-emails/
+
+SELECT B.Email
+FROM (
+         SELECT Email, COUNT(Email) AS freq
+         FROM Person
+         GROUP BY Email
+     ) AS B
+WHERE B.freq > 1;
+
+
 SELECT Email
 FROM Person
 GROUP BY Email
-HAVING COUNT(Email) > 1
+HAVING COUNT(Email) > 1;
+
