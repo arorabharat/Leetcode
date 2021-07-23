b29819136dfd95da4228108940e15ec67cd1e9b2
bharat <bharatarora3072@gmail.com>
2021-07-23 13:10:08 +0530

    Fri Jul 23 13:10:08 IST 2021

--- bin-manipulation/src/Solution_268.java
+++ bin-manipulation/src/Solution_268.java
@@ -2,4 +2,14 @@
  * https://leetcode.com/problems/missing-number/
  */
 class Solution_268 {
+
+    public int missingNumber(int[] nums) {
+        int n = nums.length;
+        int actualSum = 0;
+        for (int num : nums) {
+            actualSum += num;
+        }
+        int expectedSum = n * (n + 1) / 2;
+        return expectedSum - actualSum;
+    }
 }
