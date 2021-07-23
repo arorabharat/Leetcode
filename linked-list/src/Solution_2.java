f0101ada9f15a9b4b231117f77c5a160546ae2eb
bharat <bharatarora3072@gmail.com>
2021-07-23 13:44:52 +0530

    Fri Jul 23 13:44:52 IST 2021

--- linked-list/src/Solution_2.java
+++ linked-list/src/Solution_2.java
@@ -8,8 +8,8 @@ class Solution_2 {
         ListNode tr1 = l1;
         ListNode tr2 = l2;
         ListNode head = null;
-        ListNode res = null;
-        int c = 0;
+        ListNode tr3 = null;
+        int carry = 0;
 
         while (tr1 != null || tr2 != null) {
             int a = 0;
@@ -23,22 +23,46 @@ class Solution_2 {
                 b = tr2.val;
                 tr2 = tr2.next;
             }
-            int sum = a + b + c;
-            c = sum / 10;
+            int sum = a + b + carry;
+            carry = sum / 10;
             sum = sum % 10;
             ListNode t = new ListNode(sum);
             if (head == null) {
                 head = t;
-                res = head;
+                tr3 = head;
             } else {
-                res.next = t;
-                res = res.next;
+                tr3.next = t;
+                tr3 = tr3.next;
             }
         }
-        if (c != 0) {
-            res.next = new ListNode(c);
+        if (carry != 0) {
+            tr3.next = new ListNode(carry);
         }
         return head;
+    }
+
+
+    /**
+     * Optimizing the readability of approach 1
+     */
+    public ListNode addTwoNumbers2(ListNode l1, ListNode l2) {
+        ListNode pseudoNode = new ListNode(0);
+        ListNode tr1 = l1, tr2 = l2, tr3 = pseudoNode;
+        int carry = 0;
+        while (tr1 != null || tr2 != null) {
+            int a = (tr1 != null) ? tr1.val : 0;
+            int b = (tr2 != null) ? tr2.val : 0;
+            int sum = carry + a + b;
+            carry = sum / 10;
+            tr3.next = new ListNode(sum % 10);
+            tr3 = tr3.next;
+            if (tr1 != null) tr1 = tr1.next;
+            if (tr2 != null) tr2 = tr2.next;
+        }
+        if (carry > 0) {
+            tr3.next = new ListNode(carry);
+        }
+        return pseudoNode.next;
     }
 
     static class ListNode {
