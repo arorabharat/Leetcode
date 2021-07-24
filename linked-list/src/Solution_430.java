2fb4f44089e0b186c4c8b68e0064bce985412628
bharat <bharatarora3072@gmail.com>
2021-07-24 18:58:12 +0530

    Sat Jul 24 18:58:12 IST 2021

--- linked-list/src/Solution_430.java
+++ linked-list/src/Solution_430.java
@@ -1,3 +1,6 @@
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 /**
  * https://leetcode.com/problems/flatten-a-multilevel-doubly-linked-list/
  */
@@ -8,6 +11,13 @@ class Solution_430 {
         public Node prev;
         public Node next;
         public Node child;
+
+        public Node(int val, Node prev, Node next, Node child) {
+            this.val = val;
+            this.prev = prev;
+            this.next = next;
+            this.child = child;
+        }
     }
 
     public Node flatten(Node head) {
@@ -31,5 +41,35 @@ class Solution_430 {
             next.prev = curr;
         }
         return head;
+    }
+
+    /**
+     * iterative approach
+     * we add the next and child element in the stack
+     * keep linking the curr / top element of hte stack to the prev element / element last added to the final list
+     */
+    public Node flatten2(Node head) {
+        if (head == null) return head;
+        Node pseudoHead = new Node(0, null, head, null);
+        Node curr = null;
+        Node finalList = pseudoHead;
+        Deque<Node> stack = new ArrayDeque<>();
+        stack.push(head);
+        while (!stack.isEmpty()) {
+            curr = stack.pop();
+            finalList.next = curr;
+            curr.prev = finalList;
+            if (curr.next != null) {
+                stack.push(curr.next);
+            }
+            if (curr.child != null) {
+                stack.push(curr.child);
+                curr.child = null;
+            }
+            finalList = curr;
+        }
+        // detach the pseudo node from the result
+        pseudoHead.next.prev = null;
+        return pseudoHead.next;
     }
 }
