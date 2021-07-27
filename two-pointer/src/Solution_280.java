bd094f646c019267015e75367c545b9e7d6a3c04
bharat <bharatarora3072@gmail.com>
2021-07-27 20:00:19 +0530

    Tue Jul 27 20:00:19 IST 2021

--- two-pointer/src/Solution_280.java
+++ two-pointer/src/Solution_280.java
@@ -1,9 +1,27 @@
+import java.util.Arrays;
+
 /**
  * https://leetcode.com/problems/wiggle-sort/
- * TODO
+ * TODO : implement without sorting thr array
  */
 class Solution_280 {
 
+    /**
+     * Easiset approach is to sort the array and put the elements
+     */
     public void wiggleSort(int[] nums) {
+        int n = nums.length;
+        int[] ans = new int[n];
+        System.arraycopy(nums, 0, ans, 0, n);
+        Arrays.sort(ans);
+        int k = n - 1;
+        for (int i = 1; i < n; i = i + 2) {
+            nums[i] = ans[k];
+            k--;
+        }
+        for (int i = 0; i < n; i = i + 2) {
+            nums[i] = ans[k];
+            k--;
+        }
     }
 }
