52b67fb3e1ecf333190d37194124af6015bf11e6
bharat <bharatarora3072@gmail.com>
2021-08-03 19:58:04 +0530

    Tue Aug  3 19:58:04 IST 2021

---
+++ dynamic-programming/src/Solution_198.java
@@ -0,0 +1,35 @@
+/**
+ * https://leetcode.com/problems/house-robber/
+ */
+class Solution_198 {
+    /**
+     * Recursive approach
+     */
+    private int _rob(int[] nums, int n) {
+        if (n <= 0) {
+            return 0;
+        }
+        return Math.max(nums[n - 1] + _rob(nums, n - 2), _rob(nums, n - 1));
+    }
+
+    public int rob(int[] nums) {
+        return _rob(nums, nums.length);
+    }
+
+    /**
+     * Dynamic programming
+     */
+    public int rob2(int[] nums) {
+        int n = nums.length;
+        int[] dp = new int[n];
+        if (n == 0) return 0;
+        if (n == 1) return nums[0];
+        dp[0] = nums[0];
+        dp[1] = Math.max(nums[0], nums[1]);
+        for (int i = 2; i < n; i++) {
+            dp[i] = Math.max(nums[i] + dp[i - 2], dp[i - 1]);
+        }
+        return dp[n - 1];
+    }
+
+}
