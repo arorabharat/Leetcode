3eff13c3fb5e38cb41585269649f5a01fa287bab
bharat <bharatarora3072@gmail.com>
2021-08-07 13:06:26 +0530

    Sat Aug  7 13:06:26 IST 2021

--- dynamic-programming/src/Solution_877.java
+++ dynamic-programming/src/Solution_877.java
@@ -5,23 +5,32 @@ class Solution_877 {
 
     private int[][] memo;
 
+    /**
+     * this method return the maximum sum a person who make the first move can get making alternative move
+     * Case 1 : If I pick left then
+     * we can get the maximum for the remaining range which would go the opponent
+     * Hence I would get sum - maximum for remaining range.
+     * Case 2 : If I pick right then
+     * we can get the maximum for the remaining range which would go the opponent
+     * Hence I would get sum - maximum for remaining range
+     */
     private int _stoneGame(int[] piles, int s, int e, int sum) {
-        if (s <= e) {
+        if (s < e) {
             if (memo[s][e] != 0) {
                 return memo[s][e];
             }
             int leftPick = _stoneGame(piles, s + 1, e, sum - piles[s]);
             int rightPick = _stoneGame(piles, s, e - 1, sum - piles[e]);
-            if (piles[s] + leftPick > sum - piles[s] - leftPick) {
-                memo[s][e] = piles[s] + leftPick;
-                return piles[s] + leftPick;
+            if (sum - leftPick > leftPick) {
+                memo[s][e] = sum - leftPick;
+                return sum - leftPick;
             }
-            if (piles[e] + rightPick > sum - piles[e] - rightPick) {
-                memo[s][e] = piles[e] + rightPick;
-                return piles[e] + rightPick;
+            if (sum - rightPick > rightPick) {
+                memo[s][e] = sum - rightPick;
+                return sum - rightPick;
             }
         }
-        return 0;
+        return piles[s];
     }
 
     public boolean stoneGame(int[] piles) {
@@ -31,7 +40,6 @@ class Solution_877 {
         }
         int n = piles.length;
         memo = new int[501][501];
-        _stoneGame(piles, 0, n - 1, sum);
-        return memo[0][n - 1] > memo[1][n - 2] || memo[0][n - 1] > memo[0][n - 2]; // pick left or pick right
+        return sum - memo[1][n - 1] > memo[1][n - 1] || sum - memo[0][n - 2] > memo[0][n - 2]; // pick left or pick right
     }
 }
