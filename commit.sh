82d7455424da4d091bda9c089d9e9795deaf5ef1
bharat <bharatarora3072@gmail.com>
2021-08-11 20:36:30 +0530

    Wed Aug 11 20:36:30 IST 2021

--- commit.sh
+++ commit.sh
@@ -1,7 +1,9 @@
 echo -e "$(pwd) "
 echo -e "\e[102m Committing the changes \e[49m"
-echo -e ""
 git add .
+echo -e "\e[94m"
 git commit -m "`date`"
+echo -e "\e[93m"
 git push
+echo -e "\e[92m"
 git status
