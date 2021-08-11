2c357089cde7606fecfbb8d17a60ccabfb7a6b54
bharat <bharatarora3072@gmail.com>
2021-08-11 20:20:36 +0530

    Wed Aug 11 20:20:36 IST 2021

--- arrays/src/Solution_56.java
+++ arrays/src/Solution_56.java
@@ -3,15 +3,21 @@ import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
 
+/**
+ * https://leetcode.com/problems/merge-intervals/
+ */
 class Solution_56 {
+
+    private static final int END = 1;
+    private static final int START = 0;
 
     private List<int[]> mergeTwo(int[] interval1, int[] interval2) {
         List<int[]> list = new ArrayList<>();
-        if (interval1[1] < interval2[0]) {
+        if (interval1[END] < interval2[START]) {
             list.add(interval1);
             list.add(interval2);
         } else {
-            list.add(new int[]{interval1[0], Math.max(interval1[1], interval2[1])});
+            list.add(new int[]{interval1[START], Math.max(interval1[END], interval2[END])});
         }
         return list;
     }
