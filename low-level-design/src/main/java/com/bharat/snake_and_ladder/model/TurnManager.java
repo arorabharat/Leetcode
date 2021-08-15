2c1114b85cfc85da6f1d70979aa94aacf167b032
bharat <bharatarora3072@gmail.com>
2021-08-15 15:26:12 +0530

    Sun Aug 15 15:26:12 IST 2021

--- low-level-design/src/main/java/com/bharat/snake_and_ladder/model/TurnManager.java
+++ low-level-design/src/main/java/com/bharat/snake_and_ladder/model/TurnManager.java
@@ -3,8 +3,12 @@ package com.bharat.snake_and_ladder.model;
 public interface TurnManager {
 
     int currentTurn();
+
     void setCurrentTurn(int turn);
+
     int nextTurn();
+
     int getNumberOfPlayers();
+
     int setNumberOfPlayers(int numberOfPlayers);
 }
