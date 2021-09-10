612fa3b39eef07044cd18f3ee23a8617740aef02
bharat <bharatarora3072@gmail.com>
2021-09-10 22:44:30 +0530

    Fri Sep 10 22:44:30 IST 2021

---
+++ arrays/src/Solution_768.java
@@ -0,0 +1,43 @@
+import java.util.Arrays;
+import java.util.TreeMap;
+
+/**
+ * https://leetcode.com/problems/max-chunks-to-make-sorted-ii/
+ * TODO : there is simple solution, try that next time 
+ */
+class Solution_768 {
+
+    TreeMap<Integer, Integer> elementCount = new TreeMap<>();
+    TreeMap<Integer, Integer> elementIndex = new TreeMap<>();
+
+    private void plusOneElementCount(int x) {
+        elementCount.put(x, elementCount.getOrDefault(x, 0) + 1);
+    }
+
+    private void incrementIndex(int x) {
+        elementCount.put(x, elementCount.getOrDefault(x, 0) + 1);
+    }
+
+    public int maxChunksToSorted(int[] arr) {
+        int n = arr.length;
+        if (n == 0) return 0;
+        Arrays.stream(arr).forEach(this::plusOneElementCount);
+        int prev = 0;
+        int next;
+        for (Integer x : elementCount.navigableKeySet()) {
+            next = elementCount.get(x);
+            elementIndex.put(x, prev);
+            prev = prev + next;
+        }
+        int partitionEnd = 0;
+        int count = 0;
+        for (int i = 0; i < n; i++) {
+            partitionEnd = Math.max(partitionEnd, elementIndex.get(arr[i]));
+            plusOneElementCount(arr[i]);
+            if (i == partitionEnd) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
