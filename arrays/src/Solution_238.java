30b5006251d455e167e0c07d645785f61a26c256
bharat <bharatarora3072@gmail.com>
2021-09-18 18:00:48 +0530

    Sat Sep 18 18:00:48 IST 2021

--- arrays/src/Solution_238.java
+++ arrays/src/Solution_238.java
@@ -2,6 +2,7 @@
  * https://leetcode.com/problems/product-of-array-except-self/
  */
 class Solution_238 {
+
     public int[] productExceptSelf(int[] nums) {
         int n = nums.length;
         int[] prefix = new int[n];
@@ -17,6 +18,21 @@ class Solution_238 {
         int[] result = new int[n];
         for (int i = 0; i < n; i++) {
             result[i] = prefix[i] * suffix[i];
+        }
+        return result;
+    }
+
+    public int[] productExceptSelf2(int[] nums) {
+        int n = nums.length;
+        int[] result = new int[n];
+        int prefix = 1;
+        result[n - 1] = 1;
+        for (int i = n - 2; i >= 0; i--) {
+            result[i] = result[i + 1] * nums[i + 1];
+        }
+        for (int i = 1; i < n; i++) {
+            prefix = prefix * nums[i - 1];
+            result[i] = result[i] * prefix;
         }
         return result;
     }
