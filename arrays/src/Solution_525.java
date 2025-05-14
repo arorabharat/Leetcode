5e8b011dffccc675a2c9862c6fb20f843b249e08
bharat <bharatarora3072@gmail.com>
2025-05-14 16:05:31 +0530

    Added solution for 525

---
+++ arrays/src/Solution_525.java
@@ -0,0 +1,24 @@
+public class Solution_525 {
+
+
+    public int findMaxLength(int[] nums) {
+        int N = nums.length;
+        int[] prefix = new int[N + 1];
+        // prefix[i] = sum of array of element less than ith index
+        // or we can say prefix i store sum of first i element.
+        prefix[0] = 0;
+        for (int i = 1; i <= N; i++) {
+            prefix[i] = prefix[i - 1] + (nums[i - 1] == 0 ? -1 : 1);
+        }
+        int maxLen = Integer.MIN_VALUE;
+        for (int i = 0; i < N; i++) {
+            for (int j = i + 1; j <= N; j++) {
+                // [i,j)
+                if (prefix[j] - prefix[i] == 0) {
+                    maxLen = Math.max(maxLen, j - i);
+                }
+            }
+        }
+        return maxLen;
+    }
+}
---
+++ high-level-design/tiny-url.md
@@ -0,0 +1,29 @@
+Design tiny url
+
+business Functional requirement
+
+1. It should be able to convert an url to tiny url.
+2. Converted tiny url should be able to return original url when requested
+3. It should be able to track the metrics.
+4. X - User 1 -> Y
+5. X - user 2 -> Z
+6. Tiny url should stay till Y years if unused.
+7. Do I have the permission to delete the URL, and how long it will take to delete URL
+8.
+
+Non functional / technical requirement
+
+1. It should have lower response time
+2. It should have 24/7 availability. CAP -> PP
+3. It should be able to handle the TPS of X.
+
+High level components
+
+
+
+
+
+
+
+
+
