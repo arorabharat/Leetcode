1372db321e5d0641ff4c9f5cf1ccf121d33f804d
bharat <bharatarora3072@gmail.com>
2025-05-14 18:38:46 +0530

    Added solution for 525

--- arrays/src/Solution_12.java
+++ arrays/src/Solution_12.java
@@ -19,7 +19,7 @@ class Solution_12 {
     public String intToRoman(int num) {
         if (num < 1) return null;
         int n = values.length;
-        StringBuilder sb = new StringBuilder("");
+        StringBuilder sb = new StringBuilder();
         for (int i = n - 1; i >= 0; i--) {
             if (num == 0) break;
             if (values[i] <= num) {
@@ -29,5 +29,49 @@ class Solution_12 {
             }
         }
         return sb.toString();
+    }
+
+    class Solution2 {
+        int getFirstDigit(int num) {
+            while (num > 9) {
+                num = num / 10;
+            }
+            return num;
+        }
+
+        public String intToRoman(int num) {
+            if (num == 0) return "";
+            int f = getFirstDigit(num);
+            if (f != 4 && f != 9) {
+                if (num >= 1000) {
+                    return "M" + intToRoman(num - 1000);
+                } else if (num >= 500) {
+                    return "D" + intToRoman(num - 500);
+                } else if (num >= 100) {
+                    return "C" + intToRoman(num - 100);
+                } else if (num >= 50) {
+                    return "L" + intToRoman(num - 50);
+                } else if (num >= 10) {
+                    return "X" + intToRoman(num - 10);
+                } else if (num >= 5) {
+                    return "V" + intToRoman(num - 5);
+                } else {
+                    return "I" + intToRoman(num - 1);
+                }
+            } else {
+                if (num >= 900) {
+                    return "CM" + intToRoman(num - 900);
+                } else if (num >= 400) {
+                    return "CD" + intToRoman(num - 400);
+                } else if (num >= 90) {
+                    return "XC" + intToRoman(num - 90);
+                } else if (num >= 40) {
+                    return "XL" + intToRoman(num - 40);
+                } else if (num >= 9) {
+                    return "IX" + intToRoman(num - 9);
+                }
+                return "IV" + intToRoman(num - 4);
+            }
+        }
     }
 }
