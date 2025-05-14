afd54121c5da559b88b153009b1da8bcc71b423d
bharat <bharatarora3072@gmail.com>
2025-05-14 16:22:00 +0530

    Added solution for 525

--- arrays/src/Solution_525.java
+++ arrays/src/Solution_525.java
@@ -1,3 +1,7 @@
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+
 public class Solution_525 {
 
 
@@ -18,6 +22,30 @@ public class Solution_525 {
                     maxLen = Math.max(maxLen, j - i);
                 }
             }
+        }
+        return maxLen;
+    }
+
+    public int findMaxLength2(int[] nums) {
+        int N = nums.length;
+        int[] prefixSumFirstIElement = new int[N + 1];
+        // prefixSumFirstIElement[i] = sum of array of element less than ith index
+        // or we can say prefixSumFirstIElement i store sum of first i element.
+        prefixSumFirstIElement[0] = 0;
+        for (int i = 1; i <= N; i++) {
+            prefixSumFirstIElement[i] = prefixSumFirstIElement[i - 1] + (nums[i - 1] == 0 ? -1 : 1);
+        }
+        Map<Integer, LinkedList<Integer>> prefixSumToIndex = new HashMap<>();
+        for (int i = 0; i < N + 1; i++) {
+            LinkedList<Integer> prefixSumIndexes = prefixSumToIndex.getOrDefault(prefixSumFirstIElement[i], new LinkedList<>());
+            prefixSumIndexes.add(i);
+            prefixSumToIndex.put(prefixSumFirstIElement[i], prefixSumIndexes);
+        }
+        int maxLen = 0;
+        for (Integer prefixSum : prefixSumToIndex.keySet()) {
+            Integer front = prefixSumToIndex.get(prefixSum).getFirst();
+            Integer back = prefixSumToIndex.get(prefixSum).getLast();
+            maxLen = Math.max(maxLen, back - front);
         }
         return maxLen;
     }
