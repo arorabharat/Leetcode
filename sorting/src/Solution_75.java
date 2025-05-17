6e0737a74787a882fd0f02a64335b35c16bee429
bharat <bharatarora3072@gmail.com>
2025-05-17 21:22:03 +0530

    Solution_75

--- sorting/src/Solution_75.java
+++ sorting/src/Solution_75.java
@@ -2,6 +2,35 @@ import java.util.Arrays;
 
 // Arrays method do not return the object, the modify the original array
 class Solution_75 {
+
+    class Solution {
+
+        public void sortColors(int[] nums) {
+            int n = nums.length;
+            int nextZeroIndex = 0;
+            for (int currIndex = 0; currIndex < n; currIndex++) {
+                if (nums[currIndex] == 0) {
+                    swap(nums, currIndex, nextZeroIndex);
+                    nextZeroIndex++;
+                }
+            }
+            int nextOneIndex = nextZeroIndex;
+            for (int currIndex = nextZeroIndex; currIndex < n; currIndex++) {
+                if (nums[currIndex] == 1) {
+                    swap(nums, currIndex, nextOneIndex);
+                    nextOneIndex++;
+                }
+            }
+        }
+
+        private void swap(int[] nums, int i, int j) {
+            int temp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = temp;
+        }
+    }
+
+
     public void sortColors(int[] nums) {
         Arrays.sort(nums);
     }
