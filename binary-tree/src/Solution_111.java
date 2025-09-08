392a4f680e41823b1d56dfcb1f67a39349fe7a9a
bharat <bharatarora3072@gmail.com>
2025-09-09 02:03:13 +0530

    Binary Search tree

--- binary-tree/src/Solution_111.java
+++ binary-tree/src/Solution_111.java
@@ -26,24 +26,29 @@ class Solution_111 {
         }
     }
 
+    // same as approach 1, but little code optimize
+    class Approach_2 {
 
-    private boolean isLeafNode(TreeNode root) {
-        if (root == null) return false;
-        return root.left == null && root.right == null;
+        private boolean isLeafNode(TreeNode root) {
+            if (root == null) return false;
+            return root.left == null && root.right == null;
+        }
+
+        public int minDepth(TreeNode root) {
+            if (root == null) return 0;
+            if (isLeafNode(root)) return 1;
+            int min = Integer.MAX_VALUE;
+            if (root.left != null) {
+                min = Math.min(min, minDepth(root.left));
+            }
+            if (root.right != null) {
+                min = Math.min(min, minDepth(root.right));
+            }
+            return min + 1;
+        }
+
     }
 
-    public int minDepth(TreeNode root) {
-        if (root == null) return 0;
-        if (isLeafNode(root)) return 1;
-        int min = Integer.MAX_VALUE;
-        if (root.left != null) {
-            min = Math.min(min, minDepth(root.left));
-        }
-        if (root.right != null) {
-            min = Math.min(min, minDepth(root.right));
-        }
-        return min + 1;
-    }
 
     /**
      * Using DFS
