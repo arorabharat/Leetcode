3c30b63b4c36d23c88a7c9cb2322608b5d069545
bharat <bharatarora3072@gmail.com>
2025-09-09 02:28:45 +0530

    XOR

--- bin-manipulation/src/Solution_268.java
+++ bin-manipulation/src/Solution_268.java
@@ -8,38 +8,45 @@
  */
 class Solution_268 {
 
-    public int missingNumber(int[] nums) {
-        int n = nums.length;
-        int actualSum = 0;
-        for (int num : nums) {
-            actualSum += num;
+    class Approach_1 {
+        public int missingNumber(int[] nums) {
+            int n = nums.length;
+            int actualSum = 0;
+            for (int num : nums) {
+                actualSum += num;
+            }
+            int expectedSum = n * (n + 1) / 2;
+            return expectedSum - actualSum;
         }
-        int expectedSum = n * (n + 1) / 2;
-        return expectedSum - actualSum;
     }
+
 
     /**
      * To prevent integer overflow in sum
      */
-    public int missingNumber2(int[] nums) {
-        int n = nums.length;
-        int diff = 0;
-        for (int i = 0; i < n; i++) {
-            diff = diff + (i + 1) - nums[i];
+    class Approach_2 {
+        public int missingNumber(int[] nums) {
+            int n = nums.length;
+            int diff = 0;
+            for (int i = 0; i < n; i++) {
+                diff = diff + (i + 1) - nums[i];
+            }
+            return diff;
         }
-        return diff;
     }
 
     /**
      * try bit manipulation
      * Only number which will not cancel itself is the number which do not exists.
      */
-    public int missingNumber3(int[] nums) {
-        int n = nums.length;
-        int xor = 0;
-        for (int i = 0; i < n; i++) {
-            xor = xor ^ (i + 1) ^ nums[i];
+    class Approach_3 {
+        public int missingNumber3(int[] nums) {
+            int n = nums.length;
+            int xor = 0;
+            for (int i = 0; i < n; i++) {
+                xor = xor ^ (i + 1) ^ nums[i];
+            }
+            return xor;
         }
-        return xor;
     }
 }
