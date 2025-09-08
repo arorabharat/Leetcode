cec008a3c7f5c66772819250c380761e21bfe6bc
bharat <bharatarora3072@gmail.com>
2025-09-08 23:50:29 +0530

    Binary Search tree

--- binary-tree/src/Solution_104.java
+++ binary-tree/src/Solution_104.java
@@ -1,3 +1,6 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  * https://leetcode.com/problems/maximum-depth-of-binary-tree/
  *
@@ -8,9 +11,45 @@
  */
 class Solution_104 {
 
-    public int maxDepth(TreeNode root) {
-        if (root == null) return 0;
-        return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+    // with recursion
+    class Approach_1 {
+
+        public int maxDepth(TreeNode root) {
+            if (root == null) return 0;
+            return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+        }
+
+    }
+
+    // without recursion
+    class Approach_2 {
+
+        public int maxDepth(TreeNode root) {
+            if (root == null) return 0;
+            int height = 0;
+            Queue<TreeNode> q = new LinkedList<>();
+            q.add(root);
+            int currLevel = 1;
+            int nextLevel = 0;
+            while (!q.isEmpty()) {
+                TreeNode currNode = q.poll();
+                currLevel--;
+                if (currNode.left != null) {
+                    q.add(currNode.left);
+                    nextLevel++;
+                }
+                if (currNode.right != null) {
+                    q.add(currNode.right);
+                    nextLevel++;
+                }
+                if (currLevel == 0) {
+                    currLevel = nextLevel;
+                    nextLevel = 0;
+                    height++;
+                }
+            }
+            return height;
+        }
     }
 
     static class TreeNode {
