040c42d330b85f6a1833034fc6df77b797ee08c5
bharat <bharatarora3072@gmail.com>
2025-09-09 02:01:30 +0530

    Binary Search tree

--- binary-tree/src/Solution_111.java
+++ binary-tree/src/Solution_111.java
@@ -8,6 +8,25 @@ import java.util.LinkedList;
  */
 class Solution_111 {
 
+
+    class Approach_1 {
+
+        public int minDepth(TreeNode root) {
+            if (root == null) return 0;
+            if (root.left == null && root.right == null) {
+                return 1;
+            }
+            if (root.left == null) {
+                return minDepth(root.right) + 1;
+            }
+            if (root.right == null) {
+                return minDepth(root.left) + 1;
+            }
+            return Math.min(minDepth(root.left), minDepth(root.right)) + 1;
+        }
+    }
+
+
     private boolean isLeafNode(TreeNode root) {
         if (root == null) return false;
         return root.left == null && root.right == null;
