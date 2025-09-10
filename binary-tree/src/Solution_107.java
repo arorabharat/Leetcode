1c5b4aa8b0175484d995db868a9c5cd983b77966
bharat <bharatarora3072@gmail.com>
2025-09-10 21:38:28 +0530

    binary tree

--- binary-tree/src/Solution_107.java
+++ binary-tree/src/Solution_107.java
@@ -31,86 +31,99 @@ class Solution_107 {
         }
     }
 
-    public List<List<Integer>> levelOrderBottom(TreeNode root) {
-        List<List<Integer>> result = new ArrayList<>();
-        if (root == null) return result;
-        Queue<TreeNode> queue = new LinkedList<>();
-        queue.add(root);
-        int currentLevel = 1;
-        int nextLevel = 0;
-        int level = 0;
-        while (!queue.isEmpty()) {
-            TreeNode front = queue.remove();
-            if (result.size() == level) {
-                result.add(new ArrayList<>());
-            }
-            result.get(level).add(front.val);
-            currentLevel--;
-            if (Objects.nonNull(front.left)) {
-                queue.add(front.left);
-                nextLevel++;
-            }
-            if (Objects.nonNull(front.right)) {
-                queue.add(front.right);
-                nextLevel++;
-            }
-            if (currentLevel == 0) {
-                currentLevel = nextLevel;
-                nextLevel = 0;
-                level++;
-            }
-        }
-        Collections.reverse(result);
-        return result;
-    }
+    // same as 1, just add in reverse order in the list
+    class Approach_2 {
 
-    public List<List<Integer>> levelOrderBottom1(TreeNode root) {
-        List<List<Integer>> levels = new ArrayList<>();
-        if (root == null) return levels;
-        List<TreeNode> nextLevel = new ArrayList<>();
-        nextLevel.add(root);
-        List<TreeNode> currLevel;
-        while (!nextLevel.isEmpty()) {
-            currLevel = nextLevel;
-            nextLevel = new ArrayList<>();
-            levels.add(new ArrayList<>());
-            for (TreeNode node : currLevel) {
-                levels.get(levels.size() - 1).add(node.val);
-                if (node.left != null)
-                    nextLevel.add(node.left);
-                if (node.right != null)
-                    nextLevel.add(node.right);
+        public List<List<Integer>> levelOrderBottom(TreeNode root) {
+            LinkedList<List<Integer>> result = new LinkedList<>();
+            if (root == null) {
+                return result;
             }
-        }
-        Collections.reverse(levels);
-        return levels;
-    }
-
-    public List<List<Integer>> levelOrderBottom2(TreeNode root) {
-        LinkedList<List<Integer>> result = new LinkedList<>();
-        if (root == null) {
+            Queue<TreeNode> queue = new LinkedList<>();
+            queue.offer(root);
+            while (!queue.isEmpty()) {
+                int size = queue.size();
+                List<Integer> level = new ArrayList<>();
+                for (int i = 0; i < size; i++) {
+                    TreeNode node = queue.poll();
+                    assert node != null;
+                    level.add(node.val);
+                    if (node.left != null) {
+                        queue.offer(node.left);
+                    }
+                    if (node.right != null) {
+                        queue.offer(node.right);
+                    }
+                }
+                result.addFirst(level);
+            }
             return result;
         }
-        Queue<TreeNode> queue = new LinkedList<>();
-        queue.offer(root);
-        while (!queue.isEmpty()) {
-            int size = queue.size();
-            List<Integer> level = new ArrayList<>();
-            for (int i = 0; i < size; i++) {
-                TreeNode node = queue.poll();
-                assert node != null;
-                level.add(node.val);
-                if (node.left != null) {
-                    queue.offer(node.left);
+    }
+
+    // requires counting of current and next level.
+    class Approach_3 {
+
+        public List<List<Integer>> levelOrderBottom(TreeNode root) {
+            List<List<Integer>> result = new ArrayList<>();
+            if (root == null) return result;
+            Queue<TreeNode> queue = new LinkedList<>();
+            queue.add(root);
+            int currentLevel = 1;
+            int nextLevel = 0;
+            int level = 0;
+            while (!queue.isEmpty()) {
+                TreeNode front = queue.remove();
+                if (result.size() == level) {
+                    result.add(new ArrayList<>());
                 }
-                if (node.right != null) {
-                    queue.offer(node.right);
+                result.get(level).add(front.val);
+                currentLevel--;
+                if (Objects.nonNull(front.left)) {
+                    queue.add(front.left);
+                    nextLevel++;
+                }
+                if (Objects.nonNull(front.right)) {
+                    queue.add(front.right);
+                    nextLevel++;
+                }
+                if (currentLevel == 0) {
+                    currentLevel = nextLevel;
+                    nextLevel = 0;
+                    level++;
                 }
             }
-            result.addFirst(level);
+            Collections.reverse(result);
+            return result;
         }
-        return result;
     }
+
+    // same as 1 but uses two list one for curr and one for next level
+    class Approach_4 {
+
+        public List<List<Integer>> levelOrderBottom(TreeNode root) {
+            List<List<Integer>> levels = new ArrayList<>();
+            if (root == null) return levels;
+            List<TreeNode> nextLevel = new ArrayList<>();
+            nextLevel.add(root);
+            List<TreeNode> currLevel;
+            while (!nextLevel.isEmpty()) {
+                currLevel = nextLevel;
+                nextLevel = new ArrayList<>();
+                levels.add(new ArrayList<>());
+                for (TreeNode node : currLevel) {
+                    levels.get(levels.size() - 1).add(node.val);
+                    if (node.left != null)
+                        nextLevel.add(node.left);
+                    if (node.right != null)
+                        nextLevel.add(node.right);
+                }
+            }
+            Collections.reverse(levels);
+            return levels;
+        }
+    }
+
 
     static class TreeNode {
         int val;
