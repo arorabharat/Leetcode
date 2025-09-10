07c880eb797b2070096b03bec1320e749f670ea1
bharat <bharatarora3072@gmail.com>
2025-09-11 01:28:52 +0530

    binary tree

--- binary-tree/src/Solution_112.java
+++ binary-tree/src/Solution_112.java
@@ -1,3 +1,6 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  * https://leetcode.com/problems/path-sum/
  *
@@ -7,6 +10,39 @@
  * @see Solution_437
  */
 class Solution_112 {
+
+    class Approach_1 {
+
+        public boolean hasPathSum(TreeNode root, int targetSum) {
+            if (root == null) return false;
+            Queue<Pair> q = new LinkedList<>();
+            q.add(new Pair(root, targetSum));
+            while (!q.isEmpty()) {
+                Pair p = q.poll();
+                TreeNode node = p.node;
+                if (node.left == null && node.right == null && p.remSum == node.val) {
+                    return true;
+                }
+                if (node.left != null) {
+                    q.add(new Pair(node.left, p.remSum - node.val));
+                }
+                if (node.right != null) {
+                    q.add(new Pair(node.right, p.remSum - node.val));
+                }
+            }
+            return false;
+        }
+
+        static class Pair {
+            TreeNode node;
+            Integer remSum;
+
+            public Pair(TreeNode node, Integer remSum) {
+                this.node = node;
+                this.remSum = remSum;
+            }
+        }
+    }
 
     private boolean isLeafNode(TreeNode root) {
         return root != null && root.left == null && root.right == null;
