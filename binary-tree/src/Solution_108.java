c06b39cc1d64c0d165b1330a93f7b7dd78b9c020
bharat <bharatarora3072@gmail.com>
2025-09-10 21:41:48 +0530

    binary tree

--- binary-tree/src/Solution_108.java
+++ binary-tree/src/Solution_108.java
@@ -3,19 +3,22 @@
  */
 public class Solution_108 {
 
-    private TreeNode _sortedArrayToBST(int[] nums, int start, int end) {
-        if (start > end) return null;
-        int mid = (start + end + 1) / 2;
-        TreeNode root = new TreeNode(nums[mid]);
-        root.left = _sortedArrayToBST(nums, start, mid - 1);
-        root.right = _sortedArrayToBST(nums, mid + 1, end);
-        return root;
+    class Approach_1 {
+        private TreeNode _sortedArrayToBST(int[] nums, int start, int end) {
+            if (start > end) return null;
+            int mid = (start + end + 1) / 2;
+            TreeNode root = new TreeNode(nums[mid]);
+            root.left = _sortedArrayToBST(nums, start, mid - 1);
+            root.right = _sortedArrayToBST(nums, mid + 1, end);
+            return root;
+        }
+
+        public TreeNode sortedArrayToBST(int[] nums) {
+            int n = nums.length;
+            return _sortedArrayToBST(nums, 0, n - 1);
+        }
     }
 
-    public TreeNode sortedArrayToBST(int[] nums) {
-        int n = nums.length;
-        return _sortedArrayToBST(nums, 0, n - 1);
-    }
 
     public static class TreeNode {
         int val;
