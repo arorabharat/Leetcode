10cb0d1a326dd79633c021b0ac26afdb7c850926
bharat <bharatarora3072@gmail.com>
2025-09-12 20:35:20 +0530

    binary tree

--- dynamic-programming/src/Solution_96.java
+++ dynamic-programming/src/Solution_96.java
@@ -1,4 +1,3 @@
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -7,53 +6,40 @@ import java.util.Map;
  */
 class Solution_96 {
 
-    /**
-     * ========================================
-     */
 
-    Map<Integer, Integer> resultsCache = new HashMap<>();
+    class Approach_1 {
 
-    public int numTrees4(int n) {
-        if (n < 0) return -1;
-        if (n == 0 || n == 1) return 1;
-        if (resultsCache.containsKey(n)) return resultsCache.get(n);
-        int count = 0;
-        for (int i = 0; i < n; i++) {
-            count = count + numTrees4(n - 1 - i) * numTrees(i);
+        final Map<Integer, Integer> preCompute = new HashMap<>();
+
+        public int numTrees(int n) {
+            if (n < 0) return -1;
+            if (n == 0) return 1;
+            if (n == 1) {
+                return 1;
+            }
+            if (preCompute.containsKey(n)) {
+                return preCompute.get(n);
+            }
+            int sum = 0;
+            for (int i = 0; i < n; i++) {
+                sum = sum + numTrees(i) * numTrees(n - 1 - i);
+            }
+            preCompute.put(n, sum);
+            return sum;
         }
-        resultsCache.put(n, count);
-        return count;
     }
 
-    /**
-     * ========================================
-     */
-
-    /**
-     * Number of unique structural BST could calculated by the following algo.
-     * Each BST will have some root, lets fix all the possible root one by one from 1 to n ( for i loop )
-     * For each given root, it is deterministic that the node smaller than i will go ont the left
-     * and greater than i will go on the right side. Now, we need to calculate the BST for the left and right tree.
-     * Dp[i]  represent the number of unique tree that can created given i unique values
-     * Example : Calculating for size 3 unique tree
-     * [left,right,root]
-     * [0,2,1]
-     * [1,1,2]
-     * [2,0,3]
-     */
-    public int numTrees(int n) {
-        if (n < 2) return n;
-        int[] dp = new int[n + 1];
-        Arrays.fill(dp, 0);
-        dp[0] = 1;
-        dp[1] = 1;
-        for (int i = 2; i <= n; i++) {
-            for (int j = 1; j <= i; j++) {
-                int left = j - 1;
-                int right = i - j;
-                dp[i] = dp[i] + dp[left] * dp[right];
+    class Approach_2 {
+        public int numTrees(int n) {
+            int[] dp = new int[n + 1];
+            dp[0] = 1;
+            dp[1] = 1;
+            for (int i = 2; i <= n; i++) {
+                for (int j = 0; j < i; j++) {
+                    dp[i] = dp[i] + dp[j] * dp[i - 1 - j];
+                }
             }
+            return dp[n];
         }
-        return dp[n];
     }
 }
