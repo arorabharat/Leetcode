dc369cb847f130838d11de6d684ee8103740e587
bharat <bharatarora3072@gmail.com>
2025-09-12 22:21:03 +0530

    binary tree

--- binary-tree/src/Solution_230.java
+++ binary-tree/src/Solution_230.java
@@ -54,51 +54,56 @@ class Solution_230 {
         }
     }
 
-    public Pair _kthSmallest(TreeNode root, int k) {
-        if (Objects.isNull(root)) return new Pair(0, null);
-        if (k == 1) new Pair(null, root.val);
-        Pair leftResult = _kthSmallest(root.left, k);
-        if (Objects.nonNull(leftResult.kElement)) return leftResult;
-        if (leftResult.count + 1 == k) return new Pair(null, root.val);
-        Pair rightResult = _kthSmallest(root.right, k - leftResult.count - 1);
-        if (Objects.nonNull(rightResult.kElement)) return rightResult;
-        return new Pair(leftResult.count + 1 + rightResult.count, null);
-    }
+    class Approach_3 {
+        int nodeCount = 0;
 
-    public int kthSmallest3(TreeNode root, int k) {
-        return _kthSmallest(root, k).kElement;
-    }
-
-    class Pair {
-        Integer count;
-        Integer kElement;
-
-        public Pair(Integer count, Integer kElement) {
-            this.count = count;
-            this.kElement = kElement;
-        }
-    }
-
-    int nodeCount = 0;
-
-    private Optional<TreeNode> inorder(TreeNode root, int k) {
-        if (root != null) {
-            Optional<TreeNode> result = inorder(root.left, k);
-            if (result.isPresent()) return result;
-            nodeCount++;
-            if (nodeCount == k) {
-                return Optional.of(root);
+        private Optional<TreeNode> inorder(TreeNode root, int k) {
+            if (root != null) {
+                Optional<TreeNode> result = inorder(root.left, k);
+                if (result.isPresent()) return result;
+                nodeCount++;
+                if (nodeCount == k) {
+                    return Optional.of(root);
+                }
+                return inorder(root.right, k);
             }
-            return inorder(root.right, k);
+            return Optional.empty();
         }
-        return Optional.empty();
+
+        public int kthSmallest(TreeNode root, int k) {
+            this.nodeCount = 0;
+            Optional<TreeNode> result = inorder(root, k);
+            return result.map(treeNode -> treeNode.val).orElse(Integer.MIN_VALUE);
+        }
     }
 
-    public int kthSmallest(TreeNode root, int k) {
-        this.nodeCount = 0;
-        Optional<TreeNode> result = inorder(root, k);
-        return result.map(treeNode -> treeNode.val).orElse(Integer.MIN_VALUE);
+    class Approach_4 {
+        public Pair _kthSmallest(TreeNode root, int k) {
+            if (Objects.isNull(root)) return new Pair(0, null);
+            if (k == 1) new Pair(null, root.val);
+            Pair leftResult = _kthSmallest(root.left, k);
+            if (Objects.nonNull(leftResult.kElement)) return leftResult;
+            if (leftResult.count + 1 == k) return new Pair(null, root.val);
+            Pair rightResult = _kthSmallest(root.right, k - leftResult.count - 1);
+            if (Objects.nonNull(rightResult.kElement)) return rightResult;
+            return new Pair(leftResult.count + 1 + rightResult.count, null);
+        }
+
+        public int kthSmallest3(TreeNode root, int k) {
+            return _kthSmallest(root, k).kElement;
+        }
+
+        class Pair {
+            Integer count;
+            Integer kElement;
+
+            public Pair(Integer count, Integer kElement) {
+                this.count = count;
+                this.kElement = kElement;
+            }
+        }
     }
+
 
     static class TreeNode {
         int val;
