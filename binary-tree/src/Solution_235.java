fd5dd407e166ae24089592602b30a3c6fa249e08
bharat <bharatarora3072@gmail.com>
2025-09-13 21:04:15 +0530

    binary tree

--- binary-tree/src/Solution_235.java
+++ binary-tree/src/Solution_235.java
@@ -5,25 +5,37 @@
  */
 class Solution_235 {
 
-    /**
-     * basically there are four case
-     * Given p and q are unique.
-     * root is equal to one of the node. as we have already traversed the above nodes and we know value exist in the subtree with current root, hence the current node will be LCS
-     * both the nodes are greater than root, then we know common lowest ancestor is in right
-     * both the nodes are smaller than root, then we know common lowest ancestor is on left
-     * or both the values are on either side of the root. then current root is the lowest common ancestor.
-     */
-    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        if (root == null) return null;
-        if (root.val == p.val || root.val == q.val) return root;
-        if (root.val < p.val && root.val < q.val) {
-            return lowestCommonAncestor(root.right, p, q);
-        } else if (p.val < root.val && q.val < root.val) {
-            return lowestCommonAncestor(root.left, p, q);
-        } else {
-            return root;
+
+    class Approach_1 {
+        /**
+         * basically there are four case
+         * Given p and q are unique.
+         * root is equal to one of the node. as we have already traversed the above nodes and we know value exist in the subtree with current root, hence the current node will be LCS
+         * both the nodes are greater than root, then we know common lowest ancestor is in right
+         * both the nodes are smaller than root, then we know common lowest ancestor is on left
+         * or both the values are on either side of the root. then current root is the lowest common ancestor.
+         */
+        public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+            if (root == null) return null;
+            if (root.val == p.val || root.val == q.val) return root;
+            if (root.val < p.val && root.val < q.val) {
+                return lowestCommonAncestor(root.right, p, q);
+            } else if (p.val < root.val && q.val < root.val) {
+                return lowestCommonAncestor(root.left, p, q);
+            } else {
+                return root;
+            }
         }
     }
+
+
+    class Approach_2 {
+
+        public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+
+        }
+    }
+
 
     static class TreeNode {
         int val;
