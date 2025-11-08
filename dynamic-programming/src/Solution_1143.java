05c610b16d7b1d5b7e18933168573fcc9fd56b9c
bharat <bharatarora3072@gmail.com>
2025-11-08 20:10:04 +0530

    Longest common subsequence

--- dynamic-programming/src/Solution_1143.java
+++ dynamic-programming/src/Solution_1143.java
@@ -57,53 +57,24 @@ class Solution_1143 {
         }
     }
 
-    static class Approach_3 {
-        // longest common subsequence
-        private static boolean match(char a, char b) {
-            return a == b;
-        }
 
-        public static void main(String[] args) {
-            Approach_3 approach3 = new Approach_3();
-            int answer = approach3.longestCommonSubsequence("mhunuzqrkzsnidwbun", "szulspmhwpazoxijwbq");
-            int answer1 = approach3.longestCommonSubsequence1("mhunuzqrkzsnidwbun", "szulspmhwpazoxijwbq");
-            System.out.println(answer);
-            System.out.println(answer1);
-        }
-
-        // f(m,n) = (match(m,n)) ? 1+f(m-1,n-1) : max( f(m-1,n), f(m,n-1) )
-        public int _longestCommonSubsequence(String text1, String text2, int m, int n) {
-            if (m < 0 || n < 0) {
-                return 0;
-            }
-            if (match(text1.charAt(m), text2.charAt(n))) {
-                return 1 + _longestCommonSubsequence(text1, text2, m - 1, n - 1);
-            } else {
-                return Math.max(_longestCommonSubsequence(text1, text2, m, n - 1), _longestCommonSubsequence(text1, text2, m - 1, n));
-            }
-        }
+    // bottom up dynamic prgramming
+    class Approach_4 {
 
         public int longestCommonSubsequence(String text1, String text2) {
-            return _longestCommonSubsequence(text1, text2, text1.length() - 1, text2.length() - 2);
-        }
-
-        public int longestCommonSubsequence1(String text1, String text2) {
-            int m = text1.length(); // abcde
-            int n = text2.length(); // ace
-            int[][] dp = new int[m + 1][n + 1];
-
-            for (int i = 0; i < m; i++) {
-                for (int j = 0; j < n; j++) {
-                    if (match(text1.charAt(i), text2.charAt(j))) {
-                        dp[i + 1][j + 1] = dp[i][j] + 1;
-                    } else {
-                        dp[i + 1][j + 1] = Math.max(dp[i + 1][j], dp[i][j + 1]);
+            int n1 = text1.length();
+            int n2 = text2.length();
+            int[][] dp = new int[n1 + 1][n2 + 1];
+            for (int i = 1; i <= n1; i++) {
+                for (int j = 1; j <= n2; j++) {
+                    dp[i][j] = Math.max(dp[i][j - 1], dp[i - 1][j]);
+                    if (text1.charAt(i - 1) == text2.charAt(j - 1)) {
+                        dp[i][j] = Math.max(dp[i][j], dp[i - 1][j - 1] + 1);
                     }
                 }
             }
-            return dp[m][n];
+            return dp[n1][n2];
         }
     }
-
 
 }
