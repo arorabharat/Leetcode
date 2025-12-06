56d26522a318679cbb0cf1ae3f4102faece6a9b9
bharat <bharatarora3072@gmail.com>
2025-12-06 14:11:51 +0530

    adding HLD fundments

---
+++ high-level-design/notes/FundamentalsOfHld.d
@@ -0,0 +1,5 @@
+1. Understand request pattern
+2. Understand critical path
+3. Understand Latency and Daily active user.
+4. Understand batching or real time processing
+5. Can some process be async
