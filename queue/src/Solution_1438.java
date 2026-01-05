aa08022d4d10d98317dd48a572767f7db5b11192
bharat <bharatarora3072@gmail.com>
2026-01-06 00:24:25 +0530

    Solution_1438

--- queue/src/Solution_1438.java
+++ queue/src/Solution_1438.java
@@ -1,7 +1,119 @@
+import java.util.Deque;
 import java.util.LinkedList;
 import java.util.Queue;
 
 public class Solution_1438 {
+
+    class Approach_1 {
+
+        class Solution {
+
+            class MinQueue {
+
+                Deque<Integer> iq = new LinkedList<>();
+                Queue<Integer> q = new LinkedList<>();
+
+                void add(int v) {
+                    q.add(v);
+                    while (!iq.isEmpty() && iq.peekLast() > v) {
+                        iq.removeLast();
+                    }
+                    iq.addLast(v);
+                }
+
+                int remove() {
+                    int front = q.remove();
+                    if (!iq.isEmpty() && front == iq.peekFirst()) {
+                        iq.removeFirst();
+                    }
+                    return front;
+                }
+
+                int min() {
+                    return (iq.isEmpty()) ? -1 : iq.peekFirst();
+                }
+
+                int size() {
+                    return q.size();
+                }
+            }
+
+            class MaxQueue {
+
+                Deque<Integer> dq = new LinkedList<>();
+                Queue<Integer> q = new LinkedList<>();
+
+                void add(int v) {
+                    q.add(v);
+                    while (!dq.isEmpty() && dq.peekLast() < v) {
+                        dq.removeLast();
+                    }
+                    dq.addLast(v);
+                }
+
+                void remove() {
+                    int front = q.remove();
+                    if (!dq.isEmpty() && front == dq.peekFirst()) {
+                        dq.removeFirst();
+                    }
+                }
+
+                int max() {
+                    return (dq.isEmpty()) ? -1 : dq.peekFirst();
+                }
+
+                int size() {
+                    return q.size();
+                }
+            }
+
+            class MinMaxQueue {
+
+                MinQueue minq = new MinQueue();
+                MaxQueue maxq = new MaxQueue();
+
+                MinMaxQueue() {
+                }
+
+                int diff() {
+                    return maxq.max() - minq.min();
+                }
+
+                void remove() {
+                    minq.remove();
+                    maxq.remove();
+                }
+
+                void add(int v) {
+                    minq.add(v);
+                    maxq.add(v);
+                }
+
+                int size() {
+                    return minq.size();
+                }
+            }
+
+            public int longestSubarray(int[] nums, int limit) {
+                MinMaxQueue minmaxq = new MinMaxQueue();
+                int n = nums.length;
+                if (n == 0) return 0;
+                int len = 0;
+                for (int v : nums) {
+                    while (minmaxq.diff() > limit) {
+                        minmaxq.remove();
+                    }
+                    len = Math.max(len, minmaxq.size());
+                    minmaxq.add(v);
+                }
+                while (minmaxq.diff() > limit) {
+                    minmaxq.remove();
+                }
+                len = Math.max(len, minmaxq.size());
+                return len;
+            }
+        }
+    }
 
     /**
      * brute force solution O(N^2)
