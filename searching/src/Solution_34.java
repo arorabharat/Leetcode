c18e8caffb58b4c785afaa0c1cba6a00ba4fd108
bharat <bharatarora3072@gmail.com>
2026-01-06 23:33:10 +0530

    lower bound and upper bound calculation simple

--- searching/src/Solution_34.java
+++ searching/src/Solution_34.java
@@ -44,6 +44,63 @@ class Solution_34 {
         ans[1] = (end >= 0 && target == nums[end]) ? end : -1;
         return ans;
     }
+
+    class Approach_1 {
+
+        private int findLeftMostIndex(int[] nums, int target) {
+            int left = 0;
+            int right = nums.length - 1;
+            int leftMostIndex = -1;
+
+            while (left <= right) {
+                int mid = left + (right - left) / 2;
+
+                if (nums[mid] > target) {
+                    right = mid - 1;
+                } else if (nums[mid] < target) {
+                    left = mid + 1;
+                } else {
+                    leftMostIndex = mid;
+                    right = mid - 1;   // keep searching left
+                }
+            }
+            return leftMostIndex;
+        }
+
+        private int findRightMostIndex(int[] nums, int target) {
+            int left = 0;
+            int right = nums.length - 1;
+            int rightMostIndex = -1;
+
+            while (left <= right) {
+                int mid = left + (right - left) / 2;
+
+                if (nums[mid] > target) {
+                    right = mid - 1;
+                } else if (nums[mid] < target) {
+                    left = mid + 1;
+                } else {
+                    rightMostIndex = mid;
+                    left = mid + 1;   // keep searching right
+                }
+            }
+            return rightMostIndex;
+        }
+
+        public int[] searchRange(int[] nums, int target) {
+            if (nums == null || nums.length == 0) {
+                return new int[]{-1, -1};
+            }
+
+            int left = findLeftMostIndex(nums, target);
+            if (left == -1) {
+                return new int[]{-1, -1};
+            }
+
+            int right = findRightMostIndex(nums, target);
+            return new int[]{left, right};
+        }
+    }
 }
 
 
