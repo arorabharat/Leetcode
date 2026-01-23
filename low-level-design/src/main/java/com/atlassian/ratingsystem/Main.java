6aca12546d511ed4841ff321cd723faf678a82c0
bharat <bharatarora3072@gmail.com>
2026-01-23 14:50:51 +0530

    Added ballot service example

--- low-level-design/src/main/java/com/atlassian/ratingsystem/Main.java
+++ low-level-design/src/main/java/com/atlassian/ratingsystem/Main.java
@@ -23,7 +23,7 @@ public class Main {
     record Stat(String name, long totalPoints) {
     }
 
-    static class BallotService {
+    static class BallotService_1 {
 
         List<String> getResults(List<Ballot> ballots) {
             Map<String, Long> leaderBoard = new HashMap<>();
@@ -52,12 +52,47 @@ public class Main {
     }
 
     public static void main(String[] args) {
-        BallotService ballotService = new BallotService();
+        BallotService_1 ballotService1 = new BallotService_1();
         List<Ballot> ballots = new ArrayList<>();
         ballots.add(new Ballot(Optional.of("bharat"), Optional.of("anukriti"), Optional.of("cheenu")));
         ballots.add(new Ballot(Optional.of("anukriti"), Optional.of("bharat"), Optional.of("cheenu")));
         ballots.add(new Ballot(Optional.of("anukriti"), Optional.of("bharat"), Optional.of("cheenu")));
-        List<String> results = ballotService.getResults(ballots);
+        List<String> results = ballotService1.getResults(ballots);
         System.out.println(results.toString());
+    }
+
+    public class BallotService2 {
+
+        record Ballot (List<String> candidates) {
+        }
+
+        public static List<String> getResults(List<Ballot> ballots) {
+            Map<String, Integer> leaderBoard = new HashMap<>();
+
+            int[] points = {3, 2, 1};
+
+            // 1. Aggregate scores
+            for (Ballot ballot : ballots) {
+                List<String> candidates = ballot.candidates;
+                for (int i = 0; i < candidates.size(); i++) {
+                    String candidate = candidates.get(i);
+                    leaderBoard.put(candidate, leaderBoard.getOrDefault(candidate, 0) + points[i]
+                    );
+                }
+            }
+
+            // 2. Sort candidates
+            List<String> result = new ArrayList<>(leaderBoard.keySet());
+
+            Comparator<String> stringComparator = (a, b) -> {
+                int scoreCompare = Integer.compare(leaderBoard.get(b), leaderBoard.get(a));
+                if (scoreCompare != 0) {
+                    return scoreCompare; // higher score first
+                }
+                return a.compareTo(b); // tie-breaker
+            };
+            result.sort(stringComparator);
+            return result;
+        }
     }
 }
