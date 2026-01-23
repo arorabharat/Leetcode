6c46e72268d52f83c68d035054278ed622bee256
bharat <bharatarora3072@gmail.com>
2026-01-23 09:37:29 +0530

    2034

--- heap/src/Solution_2034.java
+++ heap/src/Solution_2034.java
@@ -129,6 +129,50 @@ public class Solution_2034 {
         }
     }
 
+
+    class StockPrice {
+
+        private final Map<Integer, Integer> timeToPrice = new HashMap<>();
+        private final TreeMap<Integer, Set<Integer>> sortedByPrice = new TreeMap<>();
+        int latestTimestamp = -1;
+
+        public StockPrice() {
+        }
+
+        public void update(int timestamp, int price) {
+            Integer prevPrice = timeToPrice.get(timestamp);
+            timeToPrice.put(timestamp, price);
+            if (prevPrice != null) {
+                assert sortedByPrice.containsKey(prevPrice);
+                sortedByPrice.get(prevPrice).remove(timestamp);
+                if (sortedByPrice.get(prevPrice).isEmpty()) {
+                    sortedByPrice.remove(prevPrice);
+                }
+            }
+            addTimestampToSortedPriceTable(timestamp, price);
+            latestTimestamp = Math.max(latestTimestamp, timestamp);
+        }
+
+        private void addTimestampToSortedPriceTable(int timestamp, int price) {
+            if (!sortedByPrice.containsKey(price)) {
+                sortedByPrice.put(price, new HashSet<>());
+            }
+            sortedByPrice.get(price).add(timestamp);
+        }
+
+        public int current() {
+            return latestTimestamp != -1 ? 0 : timeToPrice.get(latestTimestamp);
+        }
+
+        public int minimum() {
+            return (sortedByPrice.isEmpty()) ? 0 : sortedByPrice.firstKey();
+        }
+
+        public int maximum() {
+            return (sortedByPrice.isEmpty()) ? 0 : sortedByPrice.lastKey();
+        }
+    }
+
 /**
  * Your StockPrice object will be instantiated and called as such:
  * StockPrice obj = new StockPrice();
