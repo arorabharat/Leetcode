e8394c8efad79cfeaed7c319eb925c0eb9d84995
bharat <bharatarora3072@gmail.com>
2026-03-07 19:31:26 +0530

    Solution_919

---
+++ binary-tree/src/Solution_919.java
@@ -0,0 +1,86 @@
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Solution_919 {
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    class CBTInserter {
+
+        private final TreeNode root;
+        private Queue<TreeNode> leaveNodes;
+
+        public CBTInserter(TreeNode root) {
+            this.root = root;
+            leaveNodes = new LinkedList<>();
+            bfs(root);
+        }
+
+        void bfs(TreeNode root) {
+            Queue<TreeNode> q = new LinkedList<>();
+            q.add(root);
+            // System.out.println(root.val);
+            while (!q.isEmpty()) {
+                TreeNode front = q.remove();
+                System.out.println(front.val);
+                if (front.left == null || front.right == null) {
+                    leaveNodes.add(front);
+                }
+
+                if (front.left != null && front.right != null) {
+                    q.add(front.left);
+                    q.add(front.right);
+                } else if (front.left != null) {
+                    q.add(front.left);
+                } else if(front.right != null) {
+                    q.add(front.right);
+                }
+            }
+        }
+
+        public int insert(int val) {
+            TreeNode firstLeaf = leaveNodes.peek();
+            assert firstLeaf != null;
+            TreeNode newNode = new TreeNode(val);
+            if (firstLeaf.left != null) {
+                firstLeaf.right = newNode;
+                leaveNodes.remove();
+                leaveNodes.add(newNode);
+                return firstLeaf.right.val;
+            } else {
+                firstLeaf.left = newNode;
+                leaveNodes.add(newNode);
+                return firstLeaf.left.val;
+            }
+        }
+
+        public TreeNode get_root() {
+            return this.root;
+        }
+    }
+
+/**
+ * Your CBTInserter object will be instantiated and called as such:
+ * CBTInserter obj = new CBTInserter(root);
+ * int param_1 = obj.insert(val);
+ * TreeNode param_2 = obj.get_root();
+ */
+}
