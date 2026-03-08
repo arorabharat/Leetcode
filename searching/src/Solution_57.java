d6bca682acf71c542aa672510de3ee182d30c1a4
bharat <bharatarora3072@gmail.com>
2026-03-08 23:14:22 +0530

    binary search

--- searching/src/Solution_57.java
+++ searching/src/Solution_57.java
@@ -1,11 +1,77 @@
+import java.util.ArrayList;
 import java.util.Deque;
 import java.util.LinkedList;
+import java.util.List;
 
 /**
  * https://leetcode.com/problems/insert-interval/
  * TODO : implement it using the two binary search
  */
 class Solution_57 {
+
+
+    class Solution {
+
+        int[][] intervals;
+
+        // [[1,3],
+        // [6,9]]
+        //  2 4 6 8 10
+        int binarySearch(int s, int e, int target) {
+            if (s < e) {
+                int m = (s + e) / 2;
+                if (target == intervals[m][0]) {
+                    return m;
+                } else if (intervals[m][0] < target) {
+                    return binarySearch(m + 1, e, target);
+                } else {
+                    return binarySearch(s, m - 1, target);
+                }
+            }
+            return s;
+        }
+
+        public int[][] insert(int[][] intervals, int[] newInterval) {
+            List<int[]> resultedInterval = new ArrayList<>();
+            this.intervals = intervals;
+            int n = intervals.length - 1;
+            int newStart = newInterval[0];
+            int insertTionIndex = binarySearch(0, n - 1, newStart);
+            Deque<int[]> q = new LinkedList<>();
+            if(insertTionIndex == -1) {
+                q.add(newInterval);
+            }
+            for (int i = 0 ; i < insertTionIndex; i++) {
+                q.add(intervals[i]);
+            }
+            if(!q.isEmpty()) {
+                int[] lastInterval = q.peekLast();
+                if(newInterval[0] <= lastInterval[1]) {
+                    lastInterval[1] = Math.max(newInterval[1] , lastInterval[1]);
+                } else {
+                    q.add(newInterval);
+                }
+            } else{
+                q.add(newInterval);
+            }
+            for (int i = insertTionIndex; i < n; i++) {
+                int[] lastInterval = q.peekLast();
+                if(lastInterval != null && intervals[i][0] <= lastInterval[1]) {
+                    lastInterval[1] = Math.max(intervals[i][1] , lastInterval[1]);
+                } else {
+                    q.add(intervals[i]);
+                }
+            }
+            int rs = q.size();
+            int[][] resultArr = new int[rs][2];
+            int i = 0;
+            for (int[] interval : resultedInterval) {
+                resultArr[i] = interval;
+            }
+            return resultArr;
+        }
+    }
+
 
     /**
      * @see DSA#BINARY_SEARCH
