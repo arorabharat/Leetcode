ac97b18697be47cfedfac1262c799eccb84b6ae6
bharat <bharatarora3072@gmail.com>
2026-03-09 17:43:16 +0530

    binary search

--- binary-tree/src/Solution_378.java
+++ binary-tree/src/Solution_378.java
@@ -3,22 +3,12 @@ import java.util.PriorityQueue;
 
 public class Solution_378 {
 
-    //[ [1,5,9],
-    //  [10,11,13],
-    //  [12,13,15]],
-
-    //[ [1,5,9],
-    //  [10,4,13],
-    //  [12,13,9]],
-
-    //  k = 8
-
-
     class Solution1 {
 
         // max Heap approach
+        // TC - O(N^2)
+        // SC - O(K)
         public int kthSmallest(int[][] matrix, int k) {
-
             PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Comparator.reverseOrder());
             int R = matrix.length;
             int C = matrix.length;
@@ -41,7 +31,7 @@ public class Solution_378 {
 
     class Solution2 {
 
-        // min heap
+        // merge list solution
         public int kthSmallest(int[][] matrix, int k) {
             int R = matrix.length;
             Comparator<int[]> firstIndexComparator = new Comparator<int[]>() {
@@ -50,23 +40,22 @@ public class Solution_378 {
                     return Integer.compare(matrix[c1[0]][c1[1]], matrix[c2[0]][c2[1]]);
                 }
             };
-            PriorityQueue<int[]> q = new PriorityQueue<>(firstIndexComparator);
+            PriorityQueue<int[]> minHeap = new PriorityQueue<>(firstIndexComparator);
             for (int r = 0; r < R; r++) {
-                q.add(new int[]{r, 0});
+                minHeap.add(new int[]{r, 0});
             }
             for (int i = 0; i < k - 1; i++) {
-                int[] c = q.remove();
-                if(c[1] + 1 < R) {
-                    q.add(new int[]{c[0], c[1] + 1});
+                int[] c = minHeap.remove();
+                if (c[1] + 1 < R) {
+                    minHeap.add(new int[]{c[0], c[1] + 1});
                 }
             }
-            if(q.isEmpty()) {
+            if (minHeap.isEmpty()) {
                 return Integer.MIN_VALUE;
             } else {
-                int[] c = q.peek();
+                int[] c = minHeap.peek();
                 return matrix[c[0]][c[1]];
             }
-
         }
 
     }
