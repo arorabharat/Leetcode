6fee4308d0b51d942cebd59440982fbfa2c94a4a
bharat <bharatarora3072@gmail.com>
2026-03-31 19:40:20 +0530

    Add initial implementation of subsets problem using recursion in Solution_78.java

--- backtracking/src/Solution_78.java
+++ backtracking/src/Solution_78.java
@@ -1,5 +1,6 @@
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 /**
@@ -25,5 +26,39 @@ class Solution_78 {
 
     public List<List<Integer>> subsets(int[] nums) {
         return _subsets(nums, nums.length - 1);
+    }
+
+
+    class Approach2 {
+
+        class Solution {
+
+            public List<List<Integer>> _subsets(int[] nums, int end) {
+                List<List<Integer>> subsetList = new ArrayList<>();
+                if (end == 0) {
+                    subsetList.add(new ArrayList<>());
+                    List<Integer> singleElement = new ArrayList<>();
+                    singleElement.add(nums[end]);
+                    subsetList.add(singleElement);
+                    return subsetList;
+                }
+                List<List<Integer>> subsetListMinus1 = _subsets(nums, end - 1);
+                for (List<Integer> subset : subsetListMinus1) {
+                    List<Integer> copy = new ArrayList<>(List.copyOf(subset));
+                    copy.add(nums[end]);
+                    subsetList.add(copy);
+                }
+                subsetList.addAll(subsetListMinus1);
+                return subsetList;
+            }
+
+            public List<List<Integer>> subsets(int[] nums) {
+                int len = nums.length;
+                if (len == 0) {
+                    return new ArrayList<>();
+                }
+                return _subsets(nums, len - 1);
+            }
+        }
     }
 }
