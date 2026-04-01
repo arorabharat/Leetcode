8a56df5c86dd8207d3125b37dd1a63f7f60c9b66
bharat <bharatarora3072@gmail.com>
2026-04-02 00:45:27 +0530

    Solution_394

--- strings/src/Solution_394.java
+++ strings/src/Solution_394.java
@@ -1,3 +1,5 @@
+import java.util.Stack;
+
 public class Solution_394 {
 
     class Solution {
@@ -55,6 +57,50 @@ public class Solution_394 {
 
         public String decodeString(String s) {
             return _decodeString(s, 0, s.length() - 1).toString();
+        }
+    }
+
+    class Solution2 {
+
+        public String decodeString(String s) {
+
+            Stack<Integer> countStack = new Stack<>();
+            Stack<StringBuilder> strStack = new Stack<>();
+
+            StringBuilder current = new StringBuilder();
+            int k = 0;
+
+            for (char c : s.toCharArray()) {
+
+                if (Character.isDigit(c)) {
+                    k = k * 10 + (c - '0');
+
+                } else if (c == '[') {
+
+                    countStack.push(k);
+                    strStack.push(current);
+
+                    current = new StringBuilder();
+                    k = 0;
+
+                } else if (c == ']') {
+
+                    int repeat = countStack.pop();
+                    StringBuilder prev = strStack.pop();
+
+                    for (int i = 0; i < repeat; i++) {
+                        prev.append(current);
+                    }
+
+                    current = prev;
+
+                } else {
+
+                    current.append(c);
+                }
+            }
+
+            return current.toString();
         }
     }
 }
