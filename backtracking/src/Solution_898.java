9feb3be0f3cee93624a2eae7688645699f6e0f16
bharat <bharatarora3072@gmail.com>
2026-04-02 20:50:55 +0530

    Refactor bitwise operations in Solution_898.java and add a new implementation for subarray bitwise ORs

--- backtracking/src/Solution_898.java
+++ backtracking/src/Solution_898.java
@@ -3,23 +3,53 @@ import java.util.Set;
 
 public class Solution_898 {
 
-    Set<Integer> bitwise(int[] arr, int s, int e) {
-        if (s > e) {
-            throw new RuntimeException("Invalid range");
+    class Solution {
+
+        private Set<Integer> globalSet;
+
+        Set<Integer> bitwise(int[] arr, int s, int e) {
+            if (s > e) {
+                throw new RuntimeException("Invalid range");
+            }
+            if (s == e) {
+                globalSet.add(arr[s]);
+                return Set.of(arr[s]);
+            }
+            Set<Integer> subsetBitwise = bitwise(arr, s + 1, e);
+            Set<Integer> setBitwise = new HashSet<>();
+            for (int subsetNum : subsetBitwise) {
+                int uniqueValue = subsetNum | arr[s];
+                setBitwise.add(uniqueValue);
+            }
+            setBitwise.add(arr[s]);
+            globalSet.addAll(setBitwise);
+            return setBitwise;
         }
-        if (s == e) {
-            return new HashSet<>(arr[s]);
+
+        public int subarrayBitwiseORs(int[] arr) {
+            globalSet = new HashSet<>();
+            bitwise(arr, 0, arr.length - 1);
+            return globalSet.size();
         }
-        Set<Integer> subsetBitwise = bitwise(arr, s + 1, e);
-        Set<Integer> setBitwise = new HashSet<>(subsetBitwise);
-        for (int uv : subsetBitwise) {
-            int nuv = uv * arr[s];
-            setBitwise.add(nuv);
-        }
-        return setBitwise;
     }
 
-    public int subarrayBitwiseORs(int[] arr) {
-        return bitwise(arr, 0, arr.length - 1).size();
+    class Solution2 {
+
+        public int subarrayBitwiseORs(int[] arr) {
+            Set<Integer> allSubsetUniqueElementSet = new HashSet<>();
+            Set<Integer> lastSubsetUniqueElementSet = new HashSet<>();
+            for (int num : arr) {
+                Set<Integer> currSubsetUniqueElementSet = new HashSet<>();
+                for (Integer lastSubsetUniqueElement : lastSubsetUniqueElementSet) {
+                    int currSubsetUniqueElement = num | lastSubsetUniqueElement;
+                    currSubsetUniqueElementSet.add(currSubsetUniqueElement);
+                }
+                currSubsetUniqueElementSet.add(num);
+                lastSubsetUniqueElementSet = currSubsetUniqueElementSet;
+                allSubsetUniqueElementSet.addAll(lastSubsetUniqueElementSet);
+            }
+            return allSubsetUniqueElementSet.size();
+        }
     }
+
 }
