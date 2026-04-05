a4e7420601ce49e374b7a7c1f5c4deaf9ed20e58
bharat <bharatarora3072@gmail.com>
2026-04-05 22:27:25 +0530

    Add implementation for minimum cost tickets using dynamic programming in Solution_983.java

--- dynamic-programming/src/Solution_983.java
+++ dynamic-programming/src/Solution_983.java
@@ -1,4 +1,7 @@
 import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 
 public class Solution_983 {
 
@@ -68,6 +71,74 @@ public class Solution_983 {
                 j--;
             }
             return j;
+        }
+    }
+
+    class Solution2 {
+        public int mincostTickets(int[] days, int[] costs) {
+            // The last day on which we need to travel.
+            int lastDay = days[days.length - 1];
+            int[] dp = new int[lastDay + 1];
+            Arrays.fill(dp, 0);
+
+            int i = 0;
+            for (int day = 1; day <= lastDay; day++) {
+                // If we don't need to travel on this day, the cost won't change.
+                if (day < days[i]) {
+                    dp[day] = dp[day - 1];
+                } else {
+                    // Buy a pass on this day, and move on to the next travel day.
+                    i++;
+                    // Store the cost with the minimum of the three options.
+                    dp[day] = Math.min(dp[day - 1] + costs[0],
+                            Math.min(dp[Math.max(0, day - 7)] + costs[1],
+                                    dp[Math.max(0, day - 30)] + costs[2]));
+                }
+            }
+
+            return dp[lastDay];
+        }
+    }
+
+    class Solution3 {
+        Set<Integer> isTravelNeeded = new HashSet<>();
+
+        private int solve(int[] dp, int[] days, int[] costs, int currDay) {
+            // If we have iterated over travel days, return 0.
+            if (currDay > days[days.length - 1]) {
+                return 0;
+            }
+
+            // If we don't need to travel on this day, move on to next day.
+            if (!isTravelNeeded.contains(currDay)) {
+                return solve(dp, days, costs, currDay + 1);
+            }
+
+            // If already calculated, return from here with the stored answer.
+            if (dp[currDay] != -1) {
+                return dp[currDay];
+            }
+
+            int oneDay = costs[0] + solve(dp, days, costs, currDay + 1);
+            int sevenDay = costs[1] + solve(dp, days, costs, currDay + 7);
+            int thirtyDay = costs[2] + solve(dp, days, costs, currDay + 30);
+
+            // Store the cost with the minimum of the three options.
+            return dp[currDay] = Math.min(oneDay, Math.min(sevenDay, thirtyDay));
+        }
+
+        public int mincostTickets(int[] days, int[] costs) {
+            // The last day on which we need to travel.
+            int lastDay = days[days.length - 1];
+            int[] dp = new int[lastDay + 1];
+            Arrays.fill(dp, -1);
+
+            // Mark the days on which we need to travel.
+            for (int day : days) {
+                isTravelNeeded.add(day);
+            }
+
+            return solve(dp, days, costs, 1);
         }
     }
 }
