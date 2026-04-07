abee1f457a74ca13af93e4364f38138072734030
bharat <bharatarora3072@gmail.com>
2026-04-07 13:58:35 +0530

    Refactor course completion logic and improve cycle detection in Solution_207.java

--- graphs/src/Solution_207.java
+++ graphs/src/Solution_207.java
@@ -12,16 +12,6 @@ class Solution_207 {
     // using DFS cycle detection
     class Solution1 {
 
-        public boolean canFinish(int numCourses, int[][] prerequisites) {
-            Graph graph = new Graph(numCourses);
-            for (int[] prerequisite : prerequisites) {
-                int u = prerequisite[0];
-                int v = prerequisite[1];
-                graph.addDirectEdge(u, v);
-            }
-            return !graph.hasCycle();
-        }
-
         class Graph {
 
             Map<Integer, List<Integer>> adj;
@@ -64,6 +54,18 @@ class Solution_207 {
                 return false;
             }
         }
+
+        public boolean canFinish(int numCourses, int[][] prerequisites) {
+            Graph graph = new Graph(numCourses);
+            for (int[] prerequisite : prerequisites) {
+                int u = prerequisite[0];
+                int v = prerequisite[1];
+                graph.addDirectEdge(u, v);
+            }
+            return !graph.hasCycle();
+        }
+
+
     }
 
     // using khan's in degree algorithm
@@ -97,104 +99,159 @@ class Solution_207 {
         }
     }
 
+    class Solution4 {
+        
+        List<List<Integer>> g;
+        boolean[] stack;
+        boolean[] v;
 
-    List<List<Integer>> g;
-    boolean[] stack;
-    boolean[] v;
-
-    /**
-     * @see DSA#CYCLE_DETECTION_IN_GRAPH
-     */
-    boolean cycle(int s) {
-        stack[s] = true;
-        v[s] = true;
-        for (Integer c : g.get(s)) {
-            if (!v[c] && cycle(c)) {
-                stack[s] = false;
-                return true;
-            } else if (v[c] && stack[c]) {
-                stack[s] = false;
-                return true;
-            }
-        }
-        stack[s] = false;
-        return false;
-    }
-
-    /**
-     * Stream api style , cycle detection
-     */
-    boolean cycle1(int s) {
-        stack[s] = true;
-        v[s] = true;
-        boolean result = g.get(s).stream().anyMatch(c -> !v[c] && cycle1(c) || v[c] && stack[c]);
-        stack[s] = false;
-        return result;
-    }
-
-    public boolean canFinish(int n, int[][] pre) {
-        stack = new boolean[n];
-        v = new boolean[n];
-
-        // create structure for the graph O(n)
-        g = new ArrayList<>();
-        for (int i = 0; i < n; i++) {
-            g.add(new ArrayList<>());
-        }
-
-        // add edges o(m)
-        for (int[] ints : pre) {
-            g.get(ints[1]).add(ints[0]);
-        }
-
-        // check cycle on each edge o(n)
-        for (int i = 0; i < n; i++) {
-            if (!v[i] && cycle(i)) return false;
-        }
-
-        return true;
-    }
-
-    /**
-     * @see DSA#KHANS_ALGO_OF_TOPOLOGICAL_SORT
-     * <p>
-     * Create a directed graph , where a edge A -> B represent course B can be completed once course A is complete
-     * We will also maintain the number of other course the every course is dependent on. From angle of graph that means in degree
-     * We will add all the courses with zero pending dependency as they could be completed.
-     * Once we complete a course, we iterate over all the course which were dependent on that course and reduce the numOfPending dependency count by 1.
-     */
-
-    public boolean canFinish1(int numCourses, int[][] prerequisites) {
-        int[] numOfPendingDependency = new int[numCourses];
-        List<List<Integer>> graph = new ArrayList<>();
-        for (int i = 0; i < numCourses; i++) {
-            graph.add(new ArrayList<>());
-        }
-        for (int[] pair : prerequisites) {
-            int prerequisite = pair[1];
-            int course = pair[0];
-            numOfPendingDependency[course]++;
-            graph.get(prerequisite).add(course);
-        }
-
-        int completedCourse = 0;
-        Queue<Integer> queue = new LinkedList<>();
-        for (int i = 0; i < numCourses; i++) {
-            if (numOfPendingDependency[i] == 0) {
-                completedCourse++;
-                queue.add(i);
-            }
-        }
-        while (!queue.isEmpty()) {
-            Integer course = queue.remove();
-            for (int dependentCourse : graph.get(course)) {
-                numOfPendingDependency[dependentCourse]--;
-                if (numOfPendingDependency[dependentCourse] == 0) {
-                    completedCourse++;
-                    queue.add(dependentCourse);
+        /**
+         * @see DSA#CYCLE_DETECTION_IN_GRAPH
+         */
+        boolean cycle(int s) {
+            stack[s] = true;
+            v[s] = true;
+            for (Integer c : g.get(s)) {
+                if (!v[c] && cycle(c)) {
+                    stack[s] = false;
+                    return true;
+                } else if (v[c] && stack[c]) {
+                    stack[s] = false;
+                    return true;
                 }
             }
+            stack[s] = false;
+            return false;
         }
-        return completedCourse == numCourses;
+
+        /**
+         * Stream api style , cycle detection
+         */
+        boolean cycle1(int s) {
+            stack[s] = true;
+            v[s] = true;
+            boolean result = g.get(s).stream().anyMatch(c -> !v[c] && cycle1(c) || v[c] && stack[c]);
+            stack[s] = false;
+            return result;
+        }
+
+        public boolean canFinish(int n, int[][] pre) {
+            stack = new boolean[n];
+            v = new boolean[n];
+
+            // create structure for the graph O(n)
+            g = new ArrayList<>();
+            for (int i = 0; i < n; i++) {
+                g.add(new ArrayList<>());
+            }
+
+            // add edges o(m)
+            for (int[] ints : pre) {
+                g.get(ints[1]).add(ints[0]);
+            }
+
+            // check cycle on each edge o(n)
+            for (int i = 0; i < n; i++) {
+                if (!v[i] && cycle(i)) return false;
+            }
+
+            return true;
+        }
+
+        /**
+         * @see DSA#KHANS_ALGO_OF_TOPOLOGICAL_SORT
+         * <p>
+         * Create a directed graph , where a edge A -> B represent course B can be completed once course A is complete
+         * We will also maintain the number of other course the every course is dependent on. From angle of graph that means in degree
+         * We will add all the courses with zero pending dependency as they could be completed.
+         * Once we complete a course, we iterate over all the course which were dependent on that course and reduce the numOfPending dependency count by 1.
+         */
+
+        public boolean canFinish1(int numCourses, int[][] prerequisites) {
+            int[] numOfPendingDependency = new int[numCourses];
+            List<List<Integer>> graph = new ArrayList<>();
+            for (int i = 0; i < numCourses; i++) {
+                graph.add(new ArrayList<>());
+            }
+            for (int[] pair : prerequisites) {
+                int prerequisite = pair[1];
+                int course = pair[0];
+                numOfPendingDependency[course]++;
+                graph.get(prerequisite).add(course);
+            }
+
+            int completedCourse = 0;
+            Queue<Integer> queue = new LinkedList<>();
+            for (int i = 0; i < numCourses; i++) {
+                if (numOfPendingDependency[i] == 0) {
+                    completedCourse++;
+                    queue.add(i);
+                }
+            }
+            while (!queue.isEmpty()) {
+                Integer course = queue.remove();
+                for (int dependentCourse : graph.get(course)) {
+                    numOfPendingDependency[dependentCourse]--;
+                    if (numOfPendingDependency[dependentCourse] == 0) {
+                        completedCourse++;
+                        queue.add(dependentCourse);
+                    }
+                }
+            }
+            return completedCourse == numCourses;
+        }
+    }
+
+    class Solution3 {
+
+        class Graph {
+
+            int n;
+            Map<Integer, List<Integer>> adj;
+
+            public Graph(int n) {
+                this.n = n;
+                this.adj = new HashMap<>();
+                for (int i = 0; i < n; i++) {
+                    this.adj.put(i, new ArrayList<>());
+                }
+            }
+
+            void addDirectedEdge(int u, int v) {
+                this.adj.get(u).add(v);
+            }
+
+            boolean dfs(int u, boolean[] inStack, boolean[] visited) {
+                visited[u] = true;
+                inStack[u] = true;
+                for (int v : this.adj.get(u)) {
+                    if ((!visited[v] && dfs(v, inStack, visited)) || (visited[v] && inStack[v])) {
+                        return true;
+                    }
+                }
+                inStack[u] = false;
+                return false;
+            }
+
+            boolean isCycle() {
+                boolean[] inStack = new boolean[this.n];
+                boolean[] visited = new boolean[this.n];
+                for (int u = 0; u < this.n; u++) {
+                    if (dfs(u, inStack, visited)) {
+                        return true;
+                    }
+                }
+                return false;
+            }
+
+        }
+
+        public boolean canFinish(int numCourses, int[][] prerequisites) {
+            Graph graph = new Graph(numCourses);
+            for (int[] preq : prerequisites) {
+                graph.addDirectedEdge(preq[1], preq[0]);
+            }
+            return !graph.isCycle();
+        }
     }
 }
