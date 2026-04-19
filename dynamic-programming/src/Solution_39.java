6d9871ca776416dd6376fa08d76099fd8a54ee91
bharat <bharatarora3072@gmail.com>
2026-04-19 11:55:02 +0530

    Add multiple solutions for combination sum problem with handling duplicates

--- dynamic-programming/src/Solution_39.java
+++ dynamic-programming/src/Solution_39.java
@@ -9,7 +9,7 @@ public class Solution_39 {
         private void _combinationSum(int target, List<Integer> combination, List<List<Integer>> combinationList, Set<String> uniqueSet) {
             if (target == 0) {
                 Collections.sort(combination);
-                if(!uniqueSet.contains(combination.toString())) {
+                if (!uniqueSet.contains(combination.toString())) {
                     combinationList.add(combination);
                 }
                 return;
@@ -28,6 +28,75 @@ public class Solution_39 {
             List<List<Integer>> combinationList = new ArrayList<>();
             _combinationSum(target, new ArrayList<>(), combinationList, new HashSet<>());
             return combinationList;
+        }
+    }
+
+    class Solution2 {
+
+        List<List<Integer>> combinations;
+
+        void dfs(int[] candidates, int target, List<Integer> selectedNumbers) {
+            if (target < 0) {
+            } else if (target == 0) {
+                this.combinations.add(List.copyOf(selectedNumbers));
+            } else {
+                for (int i = 0; i < combinations.size(); i++) {
+                    List<Integer> newSelectedNumbers = new ArrayList<>(selectedNumbers);
+                    selectedNumbers.add(candidates[i]);
+                    dfs(candidates, target - candidates[i], newSelectedNumbers);
+                    newSelectedNumbers.removeLast();
+                }
+            }
+        }
+
+        public List<List<Integer>> combinationSum(int[] candidates, int target) {
+            this.combinations = new ArrayList<>();
+            dfs(candidates, target, new ArrayList<>());
+            return this.combinations;
+        }
+    }
+
+    // if there are duplicates in the candidates
+    class Solution3 {
+
+        public List<List<Integer>> combinationSum(int[] candidates, int target) {
+
+            Arrays.sort(candidates); // important
+
+            List<List<Integer>> result = new ArrayList<>();
+
+            dfs(0, candidates, target, new ArrayList<>(), result);
+
+            return result;
+        }
+
+        private void dfs(int start,
+                         int[] candidates,
+                         int target,
+                         List<Integer> curr,
+                         List<List<Integer>> result) {
+
+            if (target == 0) {
+                result.add(new ArrayList<>(curr));
+                return;
+            }
+
+            for (int i = start; i < candidates.length; i++) {
+
+                // skip duplicates
+                if (i > start && candidates[i] == candidates[i-1])
+                    continue;
+
+                if (candidates[i] > target)
+                    break;
+
+                curr.add(candidates[i]);
+
+                // reuse allowed → i
+                dfs(i, candidates, target - candidates[i], curr, result);
+
+                curr.remove(curr.size() - 1);
+            }
         }
     }
 }
