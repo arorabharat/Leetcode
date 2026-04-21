cccc2585756070df2e09355e61aabbe9baf60be2
bharat <bharatarora3072@gmail.com>
2026-04-22 00:33:05 +0530

    Add Solution2 class for finding employee free time using event sorting

--- arrays/src/Solution_759.java
+++ arrays/src/Solution_759.java
@@ -3,29 +3,32 @@ import java.util.Collections;
 import java.util.List;
 
 public class Solution_759 {
+
+    class Interval {
+        public int start;
+        public int end;
+
+        public Interval() {
+        }
+
+        public Interval(int _start, int _end) {
+            start = _start;
+            end = _end;
+        }
+    }
+
     class Solution1 {
 
         class Stamp {
             int v;
+
             int o;
 
             Stamp(int v, int o) {
                 this.v = v;
                 this.o = o;
             }
-        }
 
-        class Interval {
-            public int start;
-            public int end;
-
-            public Interval() {
-            }
-
-            public Interval(int _start, int _end) {
-                start = _start;
-                end = _end;
-            }
         }
 
 
@@ -62,6 +65,57 @@ public class Solution_759 {
                 }
             }
             return free;
+        }
+    }
+
+    class Solution2 {
+
+        class Stamp {
+            int time;
+            int delta;
+
+            Stamp(int t, int d) {
+                time = t;
+                delta = d;
+            }
+        }
+
+        public List<Interval> employeeFreeTime(List<List<Interval>> schedule) {
+
+            List<Stamp> events = new ArrayList<>();
+
+            for (List<Interval> emp : schedule) {
+                for (Interval in : emp) {
+                    events.add(new Stamp(in.start, 1));
+                    events.add(new Stamp(in.end, -1));
+                }
+            }
+
+            // OPEN before CLOSE when same time
+            Collections.sort(events, (a, b) ->
+                    a.time == b.time
+                            ? b.delta - a.delta
+                            : a.time - b.time
+            );
+
+            List<Interval> res = new ArrayList<>();
+
+            int count = 0;
+            Integer freeStart = null;
+
+            for (Stamp e : events) {
+
+                int prev = count;
+                count += e.delta;
+
+                if (count == 0)
+                    freeStart = e.time;
+
+                else if (prev == 0 && freeStart != null)
+                    res.add(new Interval(freeStart, e.time));
+            }
+
+            return res;
         }
     }
 }
