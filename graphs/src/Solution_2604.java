7a4586c08c390fa93bb86bb40db637a87d1db0c1
bharat <bharatarora3072@gmail.com>
2026-04-24 00:01:02 +0530

    Add new documentation files and initial implementation for Solution_2604

---
+++ graphs/src/Solution_2604.java
@@ -0,0 +1,10 @@
+public class Solution_2604 {
+    class Solution {
+
+        // [3,6,7],
+        // [2G,3H,4G,6H,7H,9G]
+        public int minimumTime(int[] hens, int[] grains) {
+
+        }
+    }
+}
