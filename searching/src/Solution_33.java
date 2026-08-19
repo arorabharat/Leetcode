edb43d8e4b55a80f880888d13111d6e51f046873
bharat <bharatarora3072@gmail.com>
2026-08-19 20:18:00 +0530

    binary search

--- searching/src/Solution_33.java
+++ searching/src/Solution_33.java
@@ -73,4 +73,41 @@ class Solution_33 {
             return -1;
         }
     }
+
+    class Solution3 {
+
+        private int findPivotIndex(int[] nums) {
+            int left = 0, right = nums.length - 1;
+            while (left < right) {
+                int mid = left + (right - left) / 2;
+                if (nums[mid] > nums[right]) {
+                    left = mid + 1;
+                } else {
+                    right = mid;
+                }
+            }
+            return left; // Returns index of the minimum element
+        }
+
+        private int binarySearch(int[] nums, int left, int right, int target) {
+            while (left <= right) {
+                int mid = left + (right - left) / 2;
+                if (nums[mid] == target) return mid;
+                if (nums[mid] < target) left = mid + 1;
+                else right = mid - 1;
+            }
+            return -1;
+        }
+
+        public int search(int[] nums, int target) {
+            int pivot = findPivotIndex(nums);
+
+            // Search right half
+            if (target >= nums[pivot] && target <= nums[nums.length - 1]) {
+                return binarySearch(nums, pivot, nums.length - 1, target);
+            }
+            // Search left half
+            return binarySearch(nums, 0, pivot - 1, target);
+        }
+    }
 }
