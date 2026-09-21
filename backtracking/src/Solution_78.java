34c1d8657055bbfc9cf401a636e410ba45b4e9d4
bharat <bharatarora3072@gmail.com>
2026-09-21 23:55:12 +0530

    power set- backtracking

--- backtracking/src/Solution_78.java
+++ backtracking/src/Solution_78.java
@@ -8,71 +8,73 @@ import java.util.List;
  */
 class Solution_78 {
 
-    private List<List<Integer>> _subsets(int[] nums, int n) {
-        if (n < 0) {
-            List<List<Integer>> emptySet = new ArrayList<>();
-            emptySet.add(new ArrayList<>());
-            return emptySet;
+
+    class Solution1 {
+
+        private List<List<Integer>> _subsets(int[] nums, int n) {
+            if (n < 0) {
+                List<List<Integer>> emptySet = new ArrayList<>();
+                emptySet.add(new ArrayList<>());
+                return emptySet;
+            }
+            List<List<Integer>> sets = _subsets(nums, n - 1);
+            List<List<Integer>> result = new ArrayList<>(sets);
+            for (List<Integer> set : sets) {
+                List<Integer> copy = new ArrayList<>(set);
+                copy.add(nums[n]);
+                result.add(copy);
+            }
+            return result;
         }
-        List<List<Integer>> sets = _subsets(nums, n - 1);
-        List<List<Integer>> result = new ArrayList<>(sets);
-        for (List<Integer> set : sets) {
-            List<Integer> copy = new ArrayList<>(set);
-            copy.add(nums[n]);
-            result.add(copy);
+
+        public List<List<Integer>> subsets(int[] nums) {
+            return _subsets(nums, nums.length - 1);
         }
-        return result;
+
     }
 
-    public List<List<Integer>> subsets(int[] nums) {
-        return _subsets(nums, nums.length - 1);
-    }
 
+    class Solution2 {
 
-    class Approach2 {
-
-        class Solution {
-
-            public List<List<Integer>> _subsets(int[] nums, int end) {
-                List<List<Integer>> subsetList = new ArrayList<>();
-                if (end == 0) {
-                    subsetList.add(new ArrayList<>());
-                    List<Integer> singleElement = new ArrayList<>();
-                    singleElement.add(nums[end]);
-                    subsetList.add(singleElement);
-                    return subsetList;
-                }
-                List<List<Integer>> subsetListMinus1 = _subsets(nums, end - 1);
-                for (List<Integer> subset : subsetListMinus1) {
-                    List<Integer> copy = new ArrayList<>(List.copyOf(subset));
-                    copy.add(nums[end]);
-                    subsetList.add(copy);
-                }
-                subsetList.addAll(subsetListMinus1);
+        public List<List<Integer>> _subsets(int[] nums, int end) {
+            List<List<Integer>> subsetList = new ArrayList<>();
+            if (end == 0) {
+                subsetList.add(new ArrayList<>());
+                List<Integer> singleElement = new ArrayList<>();
+                singleElement.add(nums[end]);
+                subsetList.add(singleElement);
                 return subsetList;
             }
-
-            public List<List<Integer>> subsets(int[] nums) {
-                int len = nums.length;
-                if (len == 0) {
-                    return new ArrayList<>();
-                }
-                return _subsets(nums, len - 1);
+            List<List<Integer>> subsetListMinus1 = _subsets(nums, end - 1);
+            for (List<Integer> subset : subsetListMinus1) {
+                List<Integer> copy = new ArrayList<>(List.copyOf(subset));
+                copy.add(nums[end]);
+                subsetList.add(copy);
             }
+            subsetList.addAll(subsetListMinus1);
+            return subsetList;
+        }
+
+        public List<List<Integer>> subsets(int[] nums) {
+            int len = nums.length;
+            if (len == 0) {
+                return new ArrayList<>();
+            }
+            return _subsets(nums, len - 1);
         }
     }
 
     class Solution3 {
 
         private void subsetBuilder(int[] nums, int i, List<Integer> subset, List<List<Integer>> powerset) {
-            if(i >= nums.length) {
+            if (i >= nums.length) {
                 powerset.add(new ArrayList<>(subset));
-                return ;
+                return;
             }
             subset.add(nums[i]);
-            subsetBuilder(nums, i + 1, subset, powerset);
+            subsetBuilder(nums, i + 1, subset, powerset); // take
             subset.removeLast();
-            subsetBuilder(nums, i + 1, subset, powerset);
+            subsetBuilder(nums, i + 1, subset, powerset); // skip element in subset
         }
 
         public List<List<Integer>> subsets(int[] nums) {
