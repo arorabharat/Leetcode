135c05c3fb31873b3c24b59a09a4eaf436fa04cc
bharat <bharatarora3072@gmail.com>
2026-09-23 02:10:13 +0530

    Solution 540

---
+++ searching/src/Solution_540.java
@@ -0,0 +1,34 @@
+public class Solution_540 {
+
+    class Solution {
+        int binarySearch(int[] nums) {
+            int s = 0;
+            int e = nums.length - 1;
+            while (s < e) {
+
+                int m = s + (e - s) / 2;
+                // System.out.println(s+" "+e+" "+m);
+                if(m%2 == 0) {
+                    if (nums[m] == nums[m + 1]) {
+                        s = m + 1;
+                    } else {
+                        e = m;
+                    }
+                } else {
+                    if (nums[m] == nums[m - 1]) {
+                        s = m + 1;
+                    } else {
+                        e = m - 1;
+                    }
+                }
+
+            }
+
+            return nums[s];
+        }
+
+        public int singleNonDuplicate(int[] nums) {
+            return binarySearch(nums);
+        }
+    }
+}
